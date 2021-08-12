281513853dcc0d5d07eda981c741a20e8df46f96
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2021-08-13 04:48:48 +0900

    三角形の形状判定を追加

--- Geometric/Triangle.py
+++ Geometric/Triangle.py
@@ -1,6 +1,8 @@
 from Point import *
 
 class Triangle():
+    __slots__=["A","B","C","id"]
+    ep=1e-9
     def __init__(self,A,B,C):
         """ 3点 A,B,C を頂点とする三角形を生成する.
 
@@ -11,14 +13,21 @@ class Triangle():
         self.A=A
         self.B=B
         self.C=C
+        self.id=6
 
-    def __str__(self,A,B,C):
+    def __str__(self):
         return "[Triangle] {}, {}, {}".format(self.A,self.B,self.C)
 
     __repr__=__str__
 
     def area(self):
         return abs((self.B-self.A).det(self.C-self.A)/2)
+
+    def three_edges(self):
+        """ 辺 BC, CA, AB の長さを求める.
+
+        """
+        return abs(self.B-self.C),abs(self.C-self.A),abs(self.A-self.B)
 
 #=== 三角形の心
 def Center_of_Gravity(T):
@@ -28,7 +37,6 @@ def Center_of_Gravity(T):
     """
 
     return (T.A+T.B+T.C)/3
-
 
 def CircumCenter(T):
     """ 三角形 T の外心を求める.
@@ -50,10 +58,7 @@ def InnerCenter(T):
 
     T: Triangle
     """
-
-    a=abs(T.B-T.C)
-    b=abs(T.C-T.A)
-    c=abs(T.A-T.B)
+    a,b,c=T.three_edges()
     return (a*T.A+b*T.B+c*T.C)/(a+b+c)
 
 def OrthoCenter(T):
@@ -68,11 +73,83 @@ def Excenter(T):
 
     T: Triangle
     """
-    a=abs(T.B-T.C)
-    b=abs(T.C-T.A)
-    c=abs(T.A-T.B)
-
+    a,b,c=T.three_edges()
     Ea=(-a*T.A+b*T.B+c*T.C)/(-a+b+c)
     Eb=(a*T.A-b*T.B+c*T.C)/(a-b+c)
     Ec=(a*T.A+b*T.B-c*T.C)/(a+b-c)
     return [Ea,Eb,Ec]
+
+#=== 三角形の形状
+def is_Acute_Triangle(T):
+    """ 三角形 T が鋭角三角形かどうかを判定する.
+
+    T: Triangle
+    """
+
+    a,b,c=T.three_edges()
+    m=max(a,b,c)
+    return compare(a*a+b*b+c*c-2*m*m,0,T.ep)==1
+
+def is_Right_Triangle(T):
+    """ 三角形 T が直角三角形かどうかを判定する.
+
+    T: Triangle
+    """
+
+    a,b,c=T.three_edges()
+    m=max(a,b,c)
+    return compare(a*a+b*b+c*c-2*m*m,0,T.ep)==0
+
+def is_Obtuse_Triangle(T):
+    """ 三角形 T が鈍角三角形かどうかを判定する.
+
+    T: Triangle
+    """
+
+    a,b,c=T.three_edges()
+    m=max(a,b,c)
+    return compare(a*a+b*b+c*c-2*m*m,0,T.ep)==-1
+
+def Triangle_Division_by_Angle(T):
+    """ 三角形 T が鋭角か直角か鈍角かを判定する.
+
+    [Input]
+    T: Triangle
+
+    [Output]
+    1: 鋭角, 0: 直角, -1: 鈍角
+    """
+
+    a,b,c=T.three_edges()
+    m=max(a,b,c)
+    return compare(a*a+b*b+c*c-2*m*m,0,T.ep)
+
+def is_Isosceles_Triangle(T):
+    """ 三角形 T が二等辺三角形かを判定する.
+
+    [Input]
+    T: Triangle
+    """
+
+    a,b,c=T.three_edges()
+    return compare(a,b,T.ep)==0 or compare(b,c,T.ep)==0 or compare(c,a,T.ep)==0
+
+def is_Isosceles_Right_Triangle(T):
+    """ 三角形 T が直角二等辺三角形かを判定する.
+
+    [Input]
+    T: Triangle
+    """
+
+    return is_Right_Triangle(T) and is_Isosceles_Triangle(T)
+
+
+def is_Equilateral_Triangle(T):
+    """ 三角形 T が正三角形かを判定する.
+
+    [Input]
+    T: Triangle
+    """
+
+    a,b,c=T.three_edges()
+    return compare(a,b,T.ep)==0 and compare(b,c,T.ep)==0 and compare(c,a,T.ep)==0
