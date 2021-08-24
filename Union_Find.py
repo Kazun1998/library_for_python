cd7fdddbb19ded9e00bc029d944d29c83e88029f
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2021-08-25 04:16:36 +0900

    色々変更

--- Union_Find.py
+++ Union_Find.py
@@ -1,18 +1,19 @@
 class Union_Find():
-    __slots__=["n","parents","rank"]
+    __slots__=["n","parents","rank","edges"]
     def __init__(self,N):
-        """0,1,...,N-1を要素として初期化する.
+        """ 0,1,...,N-1 を要素として初期化する.
 
-        N:要素数
+        N: 要素数
         """
         self.n=N
         self.parents=[-1]*N
         self.rank=[0]*N
+        self.edges=[0]*N
 
     def find(self, x):
-        """要素xの属している族を調べる.
+        """ 要素 x の属している族を調べる.
 
-        x:要素
+        x: 要素
         """
         V=[]
         while self.parents[x]>=0:
@@ -24,18 +25,22 @@ class Union_Find():
         return x
 
     def union(self, x, y):
-        """要素x,yを同一視する.
+        """ 要素 x,y を同一視する.
 
-        x,y:要素
+        x,y: 要素
         """
         x=self.find(x)
         y=self.find(y)
 
         if x==y:
+            self.edges[x]+=1
             return
 
         if self.rank[x]<self.rank[y]:
             x,y=y,x
+
+        self.edges[x]+=self.edges[y]+1
+        self.edges[y]=0
 
         self.parents[x]+=self.parents[y]
         self.parents[y]=x
@@ -44,40 +49,64 @@ class Union_Find():
             self.rank[x]+=1
 
     def size(self, x):
-        """要素xの属している要素の数.
+        """ 要素 x の属している族の要素の数.
 
-        x:要素
+        x: 要素
         """
         return -self.parents[self.find(x)]
 
     def same(self, x, y):
-        """要素x,yは同一視されているか?
+        """ 要素 x,y は同一視されているか?
 
-        x,y:要素
+        x,y: 要素
         """
         return self.find(x) == self.find(y)
 
     def members(self, x):
-        """要素xが属している族の要素.
-        ※族の要素の個数が欲しいときはsizeを使うこと!!
+        """ 要素 x が属している族の要素.
+        ※族の要素の個数が欲しいときは size を使うこと!!
 
-        x:要素
+        x: 要素
         """
         root = self.find(x)
         return [i for i in range(self.n) if self.find(i) == root]
 
+    def edge_count(self, x):
+        """ 要素 x が属する族の辺の本数を求める.
+
+        x: 要素
+        """
+        return self.edges[self.find(x)]
+
+    def is_forest(self, x):
+        """ 要素 x が属する族が森かどうかを判定する.
+
+        x: 要素
+        """
+        return self.size(x)==self.edges[self.find(x)]+1
+
+    def forest_count(self):
+        """ 森になっている属の個数を求める.
+        """
+
+        X=0
+        for g in self.roots():
+            if self.is_forest(g):
+                X+=1
+        return X
+
     def roots(self):
-        """族の名前のリスト
+        """ 族の名前のリスト
         """
         return [i for i, x in enumerate(self.parents) if x < 0]
 
     def group_count(self):
-        """族の個数
+        """ 族の個数
         """
         return len(self.roots())
 
     def all_group_members(self):
-        """全ての族の出力
+        """ 全ての族の出力
         """
         X={r:[] for r in self.roots()}
         for k in range(self.n):
@@ -93,25 +122,32 @@ class Union_Find():
 
     def __repr__(self):
         return self.__str__()
+
+    def __getitem__(self,index):
+        return self.find(index)
+
+    def __setitem__(self,x,y):
+        self.union(x,y)
 #=================================================
 class Coloring_Union_Find():
-    def __init__(self,N,f,e=0):
-        """0,1,...,N-1を要素として初期化する.
+    __slots__=["n","parents","rank","data","merge"]
+    def __init__(self,N,merge,e):
+        """ 0,1,...,N-1 を要素として初期化する.
 
-        N:要素数
-        f:2変数関数の合成
-        e:最初の値
+        N: 要素数
+        merge: 合成の方法
+        e: 最初の値
         """
         self.n=N
         self.parents=[-1]*N
         self.data=[e]*N
         self.rank=[0]*N
-        self.f=f
+        self.merge=merge
 
     def find(self, x):
-        """要素xの属している族を調べる.
+        """ 要素 x の属している族を調べる.
 
-        x:要素
+        x: 要素
         """
         V=[]
         while self.parents[x]>=0:
@@ -123,9 +159,9 @@ class Coloring_Union_Find():
         return x
 
     def union(self, x, y):
-        """要素x,yを同一視する.
+        """ 要素 x,y を同一視し, それぞれが持っている族の色を統合する.
 
-        x,y:要素
+        x,y: 要素
         """
         x=self.find(x)
         y=self.find(y)
@@ -133,7 +169,7 @@ class Coloring_Union_Find():
         if x==y:
             return
 
-        self.data[x]=self.data[y]=self.f(self.data[x],self.data[y])
+        self.data[x]=self.data[y]=self.merge(self.data[x],self.data[y])
 
         if self.rank[x]<self.rank[y]:
             x,y=y,x
@@ -145,56 +181,55 @@ class Coloring_Union_Find():
             self.rank[x]+=1
 
     def size(self, x):
-        """要素xの属している要素の数.
+        """ 要素 x の属している族の要素の数.
 
-        x:要素
+        x: 要素
         """
         return -self.parents[self.find(x)]
 
     def same(self, x, y):
-        """要素x,yは同一視されているか?
+        """ 要素 x,y は同一視されているか?
 
-        x,y:要素
+        x,y: 要素
         """
         return self.find(x) == self.find(y)
 
     def set(self,x,c):
-        """要素xの属する成分の色をcに変更する.
+        """ 要素 x の属する族の色を c に変更する.
 
-        x:要素
-        c:色
+        x: 要素
+        c: 色
         """
         self.data[self.find(x)]=c
-        return
 
     def look(self,x):
-        """要素xの属する成分の色
+        """ 要素 x の属する成分の色
 
-        x:要素
+        x: 要素
         """
         return self.data[self.find(x)]
 
     def members(self, x):
-        """要素xが属している族の要素.
-        ※族の要素の個数が欲しいときはsizeを使うこと!!
+        """ 要素 x が属している族の要素.
+        ※族の要素の個数が欲しいときは size を使うこと!!
 
-        x:要素
+        x: 要素
         """
         root = self.find(x)
         return [i for i in range(self.n) if self.find(i) == root]
 
     def roots(self):
-        """族の名前のリスト
+        """ 族の名前のリスト
         """
         return [i for i,x in enumerate(self.parents) if x < 0]
 
     def group_count(self):
-        """族の個数
+        """ 族の個数
         """
         return len(self.roots())
 
     def all_group_members(self):
-        """全ての族の出力
+        """ 全ての族の出力
         """
         X={r:[] for r in self.roots()}
         for k in range(self.n):
