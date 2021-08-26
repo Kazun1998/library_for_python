0486aab1b556c3d36c449930ec08cd5f3f22aaad
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2021-08-27 04:01:22 +0900

    説明を追加

--- Topological_Sort.py
+++ Topological_Sort.py
@@ -1,14 +1,28 @@
 class Topological_Sort:
-    def __init__(self,N):
+    def __init__(self, N: int):
+        """ N 頂点からなる空グラフを用意する.
+
+        N: int
+        """
         self.N=N
         self.arc=[[] for _ in  range(N)]
         self.rev=[[] for _ in range(N)]
 
-    def add_arc(self,source,target):
+    def add_arc(self, source: int, target: int):
+        """ 有向辺 source → taeget を追加する.
+
+        """
         self.arc[source].append(target)
         self.rev[target].append(source)
 
     def sort(self):
+        """ トポロジカルソートを求める.
+
+        [Ouput]
+        存在する → トポロジカルソート
+        存在しない → None
+        """
+
         in_deg=[len(self.rev[x]) for x in range(self.N)]
         Q=[x for x in range(self.N) if in_deg[x]==0]
 
@@ -21,7 +35,10 @@ class Topological_Sort:
                 in_deg[v]-=1
                 if in_deg[v]==0:
                     Q.append(v)
-        return S
+
+        return S if len(S)==self.N else None
 
     def is_DAG(self):
-        return len(self.sort())==self.N
+        """ DAG かどうかを判定する.
+        """
+        return self.sort()!=None
