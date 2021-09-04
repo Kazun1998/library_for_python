9fd7962a69bac7ec8fddc736b238dd02da2a81ba
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2021-09-04 16:54:29 +0900

    素数の個数を実装

--- Integer.py
+++ Integer.py
@@ -411,6 +411,65 @@ def Faster_Prime_Factorization(N,L):
         D.append([a,k])
     return D
 
+#素数の個数
+#Thanks for pyranine
+#URL: https://judge.yosupo.jp/submission/31819
+def Prime_Pi(N):
+    """ N 以下の素数の個数
+
+    N: int
+    """
+
+    if N<2: return 0
+    v = int(N ** 0.5) + 1
+    smalls = [i // 2 for i in range(1, v + 1)]
+    smalls[1] = 0
+    s = v // 2
+    roughs = [2 * i + 1 for i in range(s)]
+    larges = [(N // roughs[i] + 1) // 2 for i in range(s)]
+    skip = [False] * v
+
+    pc = 0
+    for p in range(3, v):
+        if smalls[p] <= smalls[p - 1]:
+            continue
+
+        q = p * p
+        pc += 1
+        if q * q > N:
+            break
+        skip[p] = True
+        for i in range(q, v, 2 * p):
+            skip[i] = True
+
+        ns = 0
+        for k in range(s):
+            i = roughs[k]
+            if skip[i]:
+                continue
+            d = i * p
+            larges[ns] = larges[k] - (larges[smalls[d] - pc] if d < v else smalls[N // d]) + pc
+            roughs[ns] = i
+            ns += 1
+        s = ns
+        for j in range((v - 1) // p, p - 1, -1):
+            c = smalls[j] - pc
+            e = min((j + 1) * p, v)
+            for i in range(j * p, e):
+                smalls[i] -= c
+
+    for k in range(1, s):
+        m = N // roughs[k]
+        s = larges[k] - (pc + k - 1)
+        for l in range(1, k):
+            p = roughs[l]
+            if p * p > m:
+                break
+            s -= smalls[m // p] - (pc + l - 1)
+        larges[0] -= s
+
+    return larges[0]
+
 #K乗リスト
 def Power_List(N,K,Mod):
     """ i=0,1,...,N における i^K (mod Mod) のリストを求める.
