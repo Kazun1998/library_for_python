68ff73b312355c321e609e3876ef45638fea659b
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2021-09-12 03:03:28 +0900

    slot 化

--- Topological_Sort.py
+++ Topological_Sort.py
@@ -1,29 +1,30 @@
 class Topological_Sort:
+    __slots__=("N","__arc","__rev")
     def __init__(self, N: int):
         """ N 頂点からなる空グラフを用意する.
 
         N: int
         """
         self.N=N
-        self.arc=[[] for _ in  range(N)]
-        self.rev=[[] for _ in range(N)]
+        self.__arc=[[] for _ in  range(N)]
+        self.__rev=[[] for _ in range(N)]
 
     def add_arc(self, source: int, target: int):
         """ 有向辺 source → taeget を追加する.
 
         """
-        self.arc[source].append(target)
-        self.rev[target].append(source)
+        self.__arc[source].append(target)
+        self.__rev[target].append(source)
 
     def sort(self):
         """ トポロジカルソートを求める.
 
         [Ouput]
-        存在する → トポロジカルソート
+        存在する → トポロジカルソート (source 側がリストの先頭)
         存在しない → None
         """
 
-        in_deg=[len(self.rev[x]) for x in range(self.N)]
+        in_deg=[len(self.__rev[x]) for x in range(self.N)]
         Q=[x for x in range(self.N) if in_deg[x]==0]
 
         S=[]
@@ -31,7 +32,7 @@ class Topological_Sort:
             u=Q.pop()
             S.append(u)
 
-            for v in self.arc[u]:
+            for v in self.__arc[u]:
                 in_deg[v]-=1
                 if in_deg[v]==0:
                     Q.append(v)
@@ -41,4 +42,17 @@ class Topological_Sort:
     def is_DAG(self):
         """ DAG かどうかを判定する.
         """
-        return self.sort()!=None
+        in_deg=[len(self.__rev[x]) for x in range(self.N)]
+        Q=[x for x in range(self.N) if in_deg[x]==0]
+
+        K=0
+        while Q:
+            u=Q.pop()
+            K+=1
+
+            for v in self.__arc[u]:
+                in_deg[v]-=1
+                if in_deg[v]==0:
+                    Q.append(v)
+
+        return K==self.N
