354aafbf2e67521769fcf4db88151b6c30dd6618
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2021-09-12 03:03:39 +0900

    半群に対応

--- Slide_Product.py
+++ Slide_Product.py
@@ -1,49 +1,100 @@
 class Slide_Product():
-    """M項の積をスライドさせながらもとめる.
+    """ 積をスライドさせながらもとめる.
     """
 
-    def __init__(self,M,calc,unit,inv):
-        """スライド積クラスを生成する.
+    __slots__=("calc", "__front", "__back", "__left", "__right", "__cnt")
 
-        M:項数, calc:2項演算, unit:単位元, inv:逆元
+    def __init__(self,calc,X=[]):
+        """スライドプロダクトクラスを生成する.
+
+        calc: 2項演算 (半群)
         ※calcについて, 左から新しい項を加えること.
         """
         from collections import deque
-        self.M=M
-        self.unit=unit
         self.calc=calc
-        self.inv=inv
-        self.queue=deque([unit]*M)
+        self.__front=deque()
+        self.__left=deque()
+        self.__back=deque()
+        self.__right=deque()
+        self.__cnt=0
 
-        self.prod=unit
+    def __str__(self):
+        if self.__front:
+            if self.__back:
+                return str(self.__front)[6:-2]+", "+str(self.__back)[7:-1]
+            else:
+                return str(self.__front)[6:-1]
+        else:
+            return str(self.__back)[6:-1]
 
-    def push(self,x,k=1):
-        """xをk回分pushする.
+    __repr__=__str__
 
-        x:value
-        k:回数
+    def __len__(self):
+        return self.__cnt
+
+    def __bool__(self):
+        return self.__cnt>0
+
+    def push(self, x, k=1):
+        """ x を k 回 push する.
+
+        x: value
+        k: 回数
         """
-        k=min(self.M,k)
-        while k>0:
-            k-=1
-            a=self.queue.popleft()
-            self.queue.append(x)
-            self.prod=self.calc(x,self.calc(self.prod,self.inv(a)))
+        self.__cnt+=1
 
-    def push_pop(self,x):
-        """xを1回pushし, 代わりにpopされたものを返す.
+        self.__back.append(x)
 
-        x:value
+        if self.__right:
+            self.__right.append(self.calc(self.__right[-1],x))
+        else:
+            self.__right.append(x)
+
+    def pop(self, k=1):
+        """ k 回 push する.
+
+        k: 回数
         """
-        a=self.queue.popleft()
-        self.queue.append(x)
-        self.prod=self.calc(x,self.calc(self.prod,self.inv(a)))
-        return a
 
-    def product(self):
-        return self.prod
+        for _ in range(min(k,self.__cnt)):
+            if not self.__front:
+                self.__right.clear()
+                while self.__back:
+                    x=self.__back.pop()
 
-    def refresh(self):
-        self.prod=self.unit
-        for x in self.queue:
-            self.prod=self.calc(x,self.prod)
+                    if self.__front:
+                        self.__left.appendleft(self.calc(x,self.__left[0]))
+                    else:
+                        self.__left.appendleft(x)
+                    self.__front.appendleft(x)
+
+            self.__front.popleft()
+            self.__left.popleft()
+
+        self.__cnt-=min(k,self.__cnt)
+
+    def product(self,default=None):
+        """ 積を求める.
+
+        default: 空のときの返り値を設定する.
+        """
+
+        if self.__front:
+            if self.__back:
+                return self.calc(self.__left[0],self.__right[-1])
+            else:
+                return self.__left[0]
+        else:
+            if self.__back:
+                return self.__right[-1]
+            else:
+                return default
+
+    def clear(self):
+        """ 初期化する. """
+
+        self.__front.clear()
+        self.__back.clear()
+        self.__left.clear()
+        self.__right.clear()
+        self.__cnt=0
