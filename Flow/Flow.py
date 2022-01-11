2f0036826c63fe9ff4496043124403567d733acc
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-01-12 02:02:59 +0900

    実装の微調整

--- Flow/Flow.py
+++ Flow/Flow.py
@@ -1,46 +1,38 @@
-#Thansk for  aaaaaaaaaa2230
-#URL:https://atcoder.jp/contests/practice2/submissions/17017319
-import sys
+#Thansk for aaaaaaaaaa2230
+#URL: https://atcoder.jp/contests/practice2/submissions/17017372
+
 from collections import deque
 class MaxFlow:
-    class Edge:
-        def __init__(self,target,cap,base,direct):
-            self.target = target
+    inf = float("inf")
+
+    class Arc:
+        def __init__(self, to, cap, base, direction):
+            self.to = to
             self.cap = cap
-            self.flow=0
-            self.base=base
-            self.direct=direct
+            self.base = base
             self.rev = None
+            self.direction=direction
 
-        def __str__(self):
-            if self.direct==1:
-                return "[S](target:{}, flow/cap:{}/{})".format(self.target,self.flow,self.base)
-            else:
-                return "[T](source:{}, flow/cap:{}/{})".format(self.target,self.flow,self.base)
+        def __repr__(self):
+            return str((self.to, "{} / {}".format(self.cap,self.base), self.direction))
 
-        __repr__=__str__
-
-    def __init__(self,N):
-        """ N 頂点からなる Flow 場を生成する.
+    def __init__(self, N):
+        """ N 頂点のフロー場を生成する.
         """
 
-        self.N = N
-        self.graph = [[] for _ in range(N)]
-        self.old_flow=0
+        self.N=N
+        self.arc = [[] for _ in range(N)]
 
-    def add_edge(self,u,v, cap):
-        """ 容量が cap である有向辺 u→v を加える.
-
-        u,v: 頂点
-        cap: 容量
+    def add_arc(self, v, w, cap):
+        """ 容量 cap の有向辺 v → w を加える.
         """
-        graph = self.graph
-        edge = self.Edge(v,cap,cap,1)
-        edge2 = self.Edge(u,0,cap,-1)
-        edge.rev = edge2
-        edge2.rev = edge
-        graph[u].append(edge)
-        graph[v].append(edge2)
+
+        a= self.Arc(w,cap,cap,1)
+        a2 = self.Arc(v,0,cap,-1)
+        a.rev = a2
+        a2.rev = a
+        self.arc[v].append(a)
+        self.arc[w].append(a2)
 
     def __bfs(self, s, t):
         level = self.level = [self.N]*self.N
@@ -49,16 +41,16 @@ class MaxFlow:
         while q:
             now = q.popleft()
             lw = level[now]+1
-            for e in self.graph[now]:
-                if e.cap and level[e.target]> lw:
-                    level[e.target] = lw
-                    if e.target == t:
+            for a in self.arc[now]:
+                if a.cap and level[a.to]> lw:
+                    level[a.to] = lw
+                    if a.to == t:
                         return True
-                    q.append(e.target)
+                    q.append(a.to)
         return False
 
     def __dfs(self, s, t, up):
-        graph = self.graph
+        arc = self.arc
         it = self.it
         level = self.level
 
@@ -69,78 +61,59 @@ class MaxFlow:
                 st.pop()
                 flow = up
                 for w in st:
-                    e = graph[w][it[w]].rev
-                    flow = min(flow, e.cap)
+                    a = arc[w][it[w]].rev
+                    flow = min(flow, a.cap)
                 for w in st:
-                    e = graph[w][it[w]]
-                    e.cap += flow
-                    e.flow-=flow
-                    e.rev.cap -= flow
-                    e.rev.flow+=flow
+                    a = arc[w][it[w]]
+                    a.cap += flow
+                    a.rev.cap -= flow
                 return flow
             lv = level[v]-1
-            while it[v] < len(graph[v]):
-                e = graph[v][it[v]]
-                re = e.rev
-                if re.cap == 0 or lv != level[e.target]:
+            while it[v] < len(arc[v]):
+                a = arc[v][it[v]]
+                ra = a.rev
+                if ra.cap == 0 or lv != level[a.to]:
                     it[v] += 1
                     continue
-                st.append(e.target)
+                st.append(a.to)
                 break
-            if it[v] == len(graph[v]):
+            if it[v] == len(arc[v]):
                 st.pop()
                 level[v] = self.N
-
         return 0
 
-    def max_flow(self,source,target,flow_limit=float("inf")):
-        """ source から target に流す最大流の大きさを出力する.
+    def max_flow(self, source, target, flow_limit=inf):
+        """ source から target に高々 flow_limit の水流を流すとき, "新たに流れる" 水流の大きさ"""
 
-        source, target: 頂点
-        flow_limit: "追加で流す" 最大流の上限
-        """
-
-        Flow=0
-        while Flow < flow_limit and self.__bfs(source,target):
-            self.it=[0]*self.N
-            while Flow<flow_limit:
-                F=self.__dfs(source,target,flow_limit-Flow)
-                if F==0:break
-                Flow+=F
-        self.old_flow+=Flow
-        return self.old_flow
+        flow = 0
+        while flow < flow_limit and self.__bfs(source, target):
+            self.it = [0]*self.N
+            while flow < flow_limit:
+                f = self.__dfs(source, target, flow_limit-flow)
+                if f == 0:
+                    break
+                flow += f
+        return flow
 
     def flow(self):
-        """ 現在の flow の状況を求める.
-
-        [Output]
-        辞書 D が返る. D[u][v] で 有効辺 u→v に流れる量を表す.
-        """
-
-        X=[{} for _ in range(self.N)]
-        for i in range(self.N):
-            x=X[i]
-            for e in self.graph[i]:
-                if e.direct==-1: continue
-                if e.target not in x: x[e.target]=0
-                x[e.target]+=e.flow
-        return X
+        F=[{} for _ in range(self.N)]
+        arc=self.arc
+        for v in range(self.N):
+            for a in arc[v]:
+                if a.direction==1:
+                    F[v][a.to]=a.base-a.cap
+        return F
 
     def min_cut(self,s):
-        """ sにおける最小カットを求める.
-
-        [Input]
-        s: sorce (s が 0 側になる)
-
-        [Output]
-        0 or 1 のリストで, 0同士と1同士で分ける.
+        """ s を 0 に含める最小カットを求める.
         """
-        visited = [1]*self.N
-        q = deque([s]); visited[s]=0
-        while q:
-            v = q.pop()
-            for e in self.graph[v]:
-                if e.cap and visited[e.target]==1:
-                    visited[e.target]=0
-                    q.append(e.target)
-        return visited
+
+        group = [1]*self.N
+        Q = deque([s])
+        while Q:
+            v = Q.pop()
+            group[v] = 0
+            for a in self.arc[v]:
+                if a.cap and group[a.to]:
+                    Q.append(a.to)
+        return group
