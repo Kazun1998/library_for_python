e31caf43a8b51ad0041cd9c19e10557df3812a73
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-01-16 16:37:47 +0900

    Mod をグローバル化, 行列とベクトルの積を実装, テンソル積を実装

--- Modulo_Matrix/Modulo_Vector.py
+++ Modulo_Matrix/Modulo_Vector.py
@@ -4,9 +4,8 @@ class Modulo_Vector_Error(Exception):
     pass
 
 class Modulo_Vector:
-    def __init__(self,v,Mod):
+    def __init__(self,v):
         self.vec=[x%Mod for x in v]
-        self.Mod=Mod
         self.size=len(v)
 
         #出力
@@ -26,18 +25,18 @@ class Modulo_Vector:
     #加法
     def __add__(self,other):
         if self.size!=other.size:
-            raise Modulo_Vector_Error("2つのベクトルのサイズが異なります.({},{})".format(self.size,other.size))
+            raise Modulo_Vector_Error("2つのベクトルのサイズが異なります. ({}, {})".format(self.size,other.size))
 
-        if self.Mod!=other.Mod:
-            raise Modulo_Vector_Error("2つのベクトルの剰余が異なります.({}, {})".format(self.Mod,other.Mod))
         v=self.vec
         w=other.vec
-        u=[v[i]+w[i] for i in range(self.size)]
-        return Modulo_Vector(u,self.Mod)
+        return Modulo_Vector([v[i]+w[i] for i in range(self.size)])
 
     #減法
-    def __sub__(self,other):
+    def __sub__(self, other):
         return self+(-other)
+
+    def __rsub__(self, other):
+        return (-self)+other
 
     #乗法
     def __mul__(self,other):
@@ -46,37 +45,35 @@ class Modulo_Vector:
     def __rmul__(self,other):
         return self.__scale__(other)
 
-    def Inverse(self):
-        pass
-
     #スカラー倍
     def __scale__(self,r):
         v=self.vec
         v=[r*x for x in v]
-        return Modulo_Vector(v,self.Mod)
+        return Modulo_Vector(v)
 
     #内積
     def inner(self,other):
         if self.size!=other.size:
             raise Modulo_Vector_Error("2つのベクトルのサイズが異なります.({},{})".format(self.size,other.size))
 
-        if self.Mod!=other.Mod:
-            raise Modulo_Vector_Error("2つのベクトルの剰余が異なります.({}, {})".format(self.Mod,other.Mod))
-
         X=0
         v=self.vec
         w=other.vec
 
-        for x,y in zip(v,w):
-            X+=x*y
-        return X%self.Mod
+        for i in range(self.size):
+            X+=v[i]*w[i]%Mod
+        return X
+
     #累乗
     def __pow__(self,n):
         pass
 
     #等号
     def __eq__(self,other):
-        return (self.vec==other.vec) and (self.Mod==other.Mod)
+        return (self.vec==other.vec)
+
+    def __len__(self):
+        return self.size
 
     #不等号
     def __neq__(self,other):
@@ -89,31 +86,61 @@ class Modulo_Vector:
     def __setitem__(self,index,val):
         assert isinstance(index,int)
         self.vec[index]=val
+
 #=================================================
-def Zero_Vector(N,Mod):
-    """N次元のゼロベクトルを出力する.
+def Zero_Vector(N):
+    """N 次元のゼロベクトルを出力する.
 
     """
-    return Modulo_Vector([0]*N,Mod)
+    return Modulo_Vector([0]*N)
 
-def Standard_Basis(N,k,Mod):
-    """N次元ベクトルの第k標準基底を出力する.
+def Standard_Basis(N, k):
+    """N 次元ベクトルの第 k 標準基底を出力する.
 
     """
-    return Modulo_Vector([1 if i==k else 0 for i in range(N)],Mod)
+    return Modulo_Vector([1 if i==k else 0 for i in range(N)])
 
-def Vectoric_Matrix(V,Mode=True):
+def Vectoric_Matrix(V, Mode=True):
     """
 
-    V:ベクトルのリスト
-    Mode:True->行ベクトル, False->列ベクトル
+    V: ベクトルのリスト
+    Mode: True → 行ベクトル, False → 列ベクトル
     """
-    assert V
-
-    m=V[0].Mod
-    assert any([1 if v.Mod==m else 0 for v in V])
 
     M=[v.vec for v in V]
-    if Mode==False:
+    if Mode==True:
         M=[list(c) for c in zip(*M)]
-    return Modulo_Matrix(M,m)
+    return Modulo_Matrix(M)
+
+def Matrix_Action(A,v):
+    """ 行列 Aとベクトル v の積 Av を求める.
+
+    A: Matrix
+    v: Vector
+    """
+
+    assert A.col==v.size
+
+    v=v.vec
+    w=[0]*A.row
+    for i in range(A.row):
+        a=A.ele[i]
+        for j in range(A.col):
+            w[i]+=a[j]*v[j]
+            w[i]%=Mod
+    return Modulo_Vector(w)
+
+def Tensor_Product(u,v):
+    """ u,v のテンソル積 u (x) v を表すベクトルを求める.
+
+    u,v: vector
+    """
+
+    M=[[0]*len(v) for _ in range(len(u))]
+
+    for i in range(len(u)):
+        Mi=M[i]
+        for j in range(len(v)):
+            Mi[j]=u[i]*v[j]
+    return Modulo_Matrix(M)
+
