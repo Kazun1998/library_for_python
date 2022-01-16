dbd2fc33588f9f80adff256f3510b7476ab6117c
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-01-16 16:36:55 +0900

    Matrix の Mod のグローバル化に対応

--- Modulo_Matrix/Modulo_Sequence.py
+++ Modulo_Matrix/Modulo_Sequence.py
@@ -1,13 +1,12 @@
 from Modulo_Matrix import *
 
 #漸化式と行列
-def Linear_Recurrence_Sequence_Value(p,x,N,Mod):
+def Linear_Recurrence_Sequence_Value(p,x,N):
     """線形漸化式の第N項を求める.
 
     p:漸化式 (d=|p| とする.)
     x:第0項から第(d-1)項までの値
     N:第N項
-    Mod:法
 
     線形漸化式は x[n+d]=p[0]x[n]+p[1]x[n+1]+...+p[d-1]x[n+d-1] とする.
     """
@@ -21,8 +20,8 @@ def Linear_Recurrence_Sequence_Value(p,x,N,Mod):
     A=[p[::-1]]
     for i in range(d-1):
         A.append([1 if j==i else 0 for j in range(d)])
-    A=Modulo_Matrix(A,Mod)
-    v=Modulo_Matrix([[y] for y in x],Mod)
+    A=Modulo_Matrix(A)
+    v=Modulo_Matrix([[y] for y in x])
 
     X=0
     aa=pow(A,N-d+1).ele[0][::-1]
@@ -32,7 +31,7 @@ def Linear_Recurrence_Sequence_Value(p,x,N,Mod):
 
     return X%Mod
 
-def Linear_Recurrence_Sequence_Matrix(p,Mod):
+def Linear_Recurrence_Sequence_Matrix(p):
     """線形漸化式から行列を作る.
 
     p:漸化式 (d=|p|)
@@ -45,4 +44,4 @@ def Linear_Recurrence_Sequence_Matrix(p,Mod):
     d=len(p)
     for i in range(d-1):
         A.append([1 if j==i else 0 for j in range(d)])
-    return Modulo_Matrix(A,Mod)
+    return Modulo_Matrix(A)
