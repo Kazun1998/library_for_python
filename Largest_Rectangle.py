c304159ec5eea3b8b8cb41ee2018c5b1dcc2d286
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-04-16 12:02:28 +0900

    ヒストグラムの最大長方形を求める関数の実装

---
+++ Largest_Rectangle.py
@@ -0,0 +1,27 @@
+def Largest_Rectangle(H, mode=0):
+    """ ヒストグラム H における最大長方形のサイズを求める.
+
+    H: リスト
+    mode: 1 のときはその範囲も求める.
+    """
+    from collections import deque
+    H.append(0)
+    S=deque([])
+    X=-1
+    l=r=-1
+    for i in range(len(H)):
+        if (not S) or H[S[-1]]<H[i]:
+            S.append(i)
+        else:
+            while S and H[S[-1]]>=H[i]:
+                j=S.pop()
+                if X<H[j]*(i-j):
+                    X=H[j]*(i-j)
+                    l=j; r=i-1
+            H[j]=H[i]
+            S.append(j)
+
+    if mode==0:
+        return X
+    else:
+        return X,(l,r)
