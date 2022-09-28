9dcaa25237d45b126d5119d09de28227ef091f13
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-09-28 11:02:10 +0900

    1次式を加算する Imos 法クラスの実装

--- Imos.py
+++ Imos.py
@@ -8,41 +8,37 @@ class Imos_1:
     def __len__(self):
         return len(self.list)-1
 
-    def add(self, F, T, C=1):
-        """閉区間 [F, T] に C を加算する.
+    def add(self, l, r, x=1):
+        """閉区間 [l, r] に x を加算する."""
 
-        F,T : int
-        C: int
-        """
-        assert 0<=F<self.__N
-        assert 0<=T<self.__N
+        assert 0<=l<self.__N
+        assert 0<=r<self.__N
 
-        if F<=T:
-            self.list[F]+=C
-            self.list[T+1]-=C
+        if l<=r:
+            self.list[l]+=x
+            self.list[r+1]-=x
 
     def cumulative_sum(self):
         """累積和を求める.
         """
-        X=self.list
-        Y=[0]*self.__N; Y[0]=X[0]
+        X=self.list.copy()[:-1]
         for i in range(1,len(self)):
-            Y[i]=Y[i-1]+X[i]
-        return Y
+            X[i]+=X[i-1]
+        return X
 
 #=================================================
 from collections import defaultdict
-class Extended_Imos_1:
+class Sparse_Imos_1:
     def __init__(self):
         self.dict=defaultdict(int)
 
-    def add(self,F,T,C=1):
-        """閉区間 [F,T] に C を加算する.
+    def add(self, l, r, x=1):
+        """閉区間 [l,r] に x を加算する.
         """
 
-        if F<=T:
-            self.dict[F]+=C
-            self.dict[T+1]-=C
+        if l<=r:
+            self.dict[l]+=x
+            self.dict[r+1]-=x
 
     def cumulative_sum(self, since, until):
         """累積和を求める.
@@ -71,35 +67,78 @@ class Extended_Imos_1:
         return Y
 
 #=================================================
+class Linear_Imos_1:
+    def __init__(self, N):
+        """ 区間 0<=t<N に対する Imos 法を準備する.
+        """
+        self.__N=N
+        self.list=[0]*(N+2)
+
+    def __len__(self):
+        return len(self.list)-2
+
+    def add(self, l, r, x=1):
+        """閉区間 [l, r] に x を加算する."""
+
+        assert 0<=l<self.__N
+        assert 0<=r<self.__N
+
+        self.add_linear(l,r,x,0)
+
+    def add_linear(self, l, r, a, b):
+        """ 閉区間 [l,r] に次のようにして加算する.
+        I[l]+=a, I[l+1]+=a+b, I[l+2]+=a+2b, ..., I[t]+=a+(t-r)b, ...,  I[r]+=a+(r-l)b
+        """
+
+        assert 0<=l<self.__N
+        assert 0<=r<self.__N
+
+        if l<=r:
+            self.list[l]+=a
+            self.list[l+1]+=-a+b
+            self.list[r+1]+=-a-(r-l+1)*b
+            self.list[r+2]+=a+(r-l)*b
+
+    def cumulative_sum(self):
+        """累積和を求める.
+        """
+        X=self.list.copy()[:len(self)]
+        for _ in range(2):
+            for i in range(1,len(self)):
+                X[i]+=X[i-1]
+        return X
+
+#=================================================
 class Imos_2:
     def __init__(self,W,H):
         self.width=W
         self.height=H
         self.list=[[0]*(W+1) for _ in range(H+1)]
 
-    def add(self,F,T,C=1):
-        """F=(Fx,Fy), T=(Tx,Ty) に対して, 閉区間 [Fx,Tx] x [Fy,Ty] にCを加算する.
+    def add(self, i0, j0, i1, j1, x=1):
+        """ 閉区間 [i0, j0] x [i1,j1] に x を加算する.
         """
-        Fx,Fy=F
-        Tx,Ty=T
 
-        self.list[Fx][Fy]+=C
-        self.list[Fx][Ty+1]-=C
-        self.list[Tx+1][Fy]-=C
-        self.list[Tx+1][Ty+1]+=C
+        self.list[i0][j0]+=x
+        self.list[i0][j1+1]-=x
+        self.list[i1+1][j0]-=x
+        self.list[i1+1][j1+1]+=x
+
+    def add_row(self, i, x):
+        """ 第 i 行 (i, *) に x を加える."""
+        self.add(i, 0, i, self.width-1, x)
+
+    def add_column(self, j, x):
+        """ 第 j 列 (*, j) に x を加える."""
+        self.add(0, j, self.height-1, j, x)
 
     def cumulative_sum(self):
-        Y=[[0]*(self.width+1) for _ in range(self.height+1)]
+        Y=[self.list[i].copy()[:-1] for i in range(self.height)]
 
-        for x in range(self.width+1):
-            S=0
-            for y in range(self.height+1):
-                S+=self.list[y][x]
-                Y[y][x]=S
-
-        for y in range(self.height+1):
-            S=0
-            for x in range(self.width+1):
-                S+=Y[y][x]
-                Y[y][x]=S
+        for _ in range(2):
+            for i in range(len(Y)):
+                y=Y[i]
+                for j in range(1,len(y)):
+                    y[j]+=y[j-1]
+            Y=[list(y) for y in zip(*Y)]
         return Y
