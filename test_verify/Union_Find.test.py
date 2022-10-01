f74c768d7171cbe8a4f64c8e2dcfd7a888cdc450
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-10-01 19:02:26 +0900

    verify コードの修正

--- test_verify/Union_Find.test.py
+++ test_verify/Union_Find.test.py
@@ -1,20 +1,24 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
 
+#==================================================
 import sys
-
-from Union_Find import Union_Find
+sys.path.append('../Union_Find/')
+from  Union_Find import Union_Find
 
 input=sys.stdin.readline
 write=sys.stdout.write
+#==================================================
+def verify():
+    N,Q=map(int,input().split())
+    U=Union_Find(N)
+    X=[]
+    for _ in range(Q):
+        t,u,v=map(int,input().split())
+        if t==0:
+            U.union(u,v)
+        else:
+            X.append(1 if U.same(u,v) else 0)
 
-N,Q=map(int,input().split())
-U=Union_Find(N)
-X=[]
-for _ in range(Q):
-    t,u,v=map(int,input().split())
-    if t==0:
-        U.union(u,v)
-    else:
-        X.append(1 if U.same(u,v) else 0)
-
-write("\n".join(map(str,X)))
+    write("\n".join(map(str,X)))
+#==================================================
+verify()
