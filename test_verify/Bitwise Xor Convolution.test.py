08b4102511be4b8e470e36292ac4591e24bff707
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-10-03 22:28:21 +0900

    Make Verify of Bitwise Xor Convolution

---
+++ test_verify/Bitwise Xor Convolution.test.py
@@ -0,0 +1,23 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/bitwise_xor_convolution
+
+#==================================================
+import sys
+sys.path.append('Convolution/')
+from XOR_Convolution import *
+
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    import sys
+    input=sys.stdin.readline
+    write=sys.stdout.write
+
+    N=int(input())
+    A=list(map(int,input().split()))
+    B=list(map(int,input().split()))
+    write(" ".join(map(str,Convolution_XOR(A,B))))
+
+#==================================================
+verify()
