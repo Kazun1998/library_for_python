9d52ca86c813ce21419535bf0c6a57226003a74a
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-10-04 00:38:56 +0900

    Make Verify of BIT

---
+++ test_verify/Binary_Indexed_Tree.test.py
@@ -0,0 +1,33 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/point_add_range_sum
+
+#==================================================
+import sys
+sys.path.append('Binary_Indexed_Tree/')
+from Binary_Indexed_Tree import Binary_Indexed_Tree
+
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    from operator import add,neg
+
+    N,Q=map(int,input().split())
+    A=list(map(int,input().split()))
+    B=Binary_Indexed_Tree(A,add,0,neg)
+
+    Ans=[]
+    for q in range(Q):
+        mode,*query=map(int,input().split())
+
+        if mode==0:
+            p,x=query
+            B.add(p, x)
+        else:
+            l,r=query
+            Ans.append(B.sum(l, r-1))
+
+    write("\n".join(map(str,Ans)))
+
+#==================================================
+verify()
