52da8e6e8d1e9c9800a35b5db2637dc49ee3464f
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-11-22 03:49:28 +0900

    modified

---
+++ test_verify/Lcm_Convolution.test.py
@@ -0,0 +1,19 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/lcm_convolution
+
+#==================================================
+import sys
+sys.path.append('../Convolution/')
+from LCM_Convolution import *
+
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    N=int(input())
+    A=[0]+list(map(int,input().split()))
+    B=[0]+list(map(int,input().split()))
+    write(" ".join(map(str,Convolution_LCM(A,B,N)[1:])))
+
+#==================================================
+verify()
