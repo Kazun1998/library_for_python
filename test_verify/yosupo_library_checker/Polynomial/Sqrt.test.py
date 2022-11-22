189bbfcd7c946dea4370c35d80aab862a9953396
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-11-23 04:03:17 +0900

    sqrt of fps の verify を実装

---
+++ test_verify/yosupo_library_checker/Polynomial/Sqrt.test.py
@@ -0,0 +1,25 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/sqrt_of_formal_power_series
+
+#==================================================
+import sys
+sys.path.append("../../../")
+from Modulo_Sequence.Modulo_Polynomial import *
+
+import sys
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    N=int(input())
+
+    A=list(map(int,input().split()))
+    P=Modulo_Polynomial(A,N)
+
+    try:
+        write(" ".join(map(str, Sqrt(P).resize(N,True))))
+    except:
+        print(-1)
+
+#==================================================
+verify()
