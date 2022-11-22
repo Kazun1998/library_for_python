df2d04ac5d52ac3d357dcbe2cdabe4486e2df4b3
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-11-22 15:54:13 +0900

    add verify of Jump

---
+++ test_verify/Jump_on_Tree.test.py
@@ -0,0 +1,29 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/jump_on_tree
+
+#==================================================
+import sys
+from Tree.Tree import Making_Tree_from_Edges
+
+input=sys.stdin.readline
+write=sys.stdout.write
+#==================================================
+def verify():
+    N,Q=map(int,input().split())
+
+    E=[]
+    for j in range(N-1):
+        a,b=map(int,input().split())
+        E.append((a,b))
+
+    T=Making_Tree_from_Edges(N, E, 0, 0)
+
+    X=[0]*Q
+    for q in range(Q):
+        s,t,i=map(int,input().split())
+        X[q]=T.jump(s,t,i)
+
+    write("\n".join(map(str,X)))
+
+
+#==================================================
+verify()
