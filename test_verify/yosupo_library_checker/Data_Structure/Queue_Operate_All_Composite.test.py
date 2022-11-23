9da37985f2a8f8016446459d2c2214d8c92f4818
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-11-23 17:03:27 +0900

    Slide Window の verify を実装

---
+++ test_verify/yosupo_library_checker/Data_Structure/Queue_Operate_All_Composite.test.py
@@ -0,0 +1,34 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/queue_operate_all_composite
+
+#==================================================
+from Slide_Window import *
+
+import sys
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    Q=int(input())
+
+    Mod=998244353
+    def calc(f,g):
+        return (f[0]*g[0]%Mod, (f[1]*g[0]+g[1])%Mod)
+
+    S=Slide_Window(calc)
+    X=[]
+    for _ in range(Q):
+        mode,*query=map(int,input().split())
+        if mode==0:
+            S.push(query)
+        elif mode==1:
+            S.pop()
+        else:
+            a,b=S.product([1,0])
+            X.append((a*query[0]+b)%Mod)
+
+    write("\n".join(map(str,X)))
+
+
+#==================================================
+verify()
