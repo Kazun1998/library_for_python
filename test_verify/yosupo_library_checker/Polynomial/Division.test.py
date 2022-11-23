3beef856e55d52825a095f3fea6aede2fefab48b
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-11-23 17:53:21 +0900

    Division of fps の verify の実装

---
+++ test_verify/yosupo_library_checker/Polynomial/Division.test.py
@@ -0,0 +1,32 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/division_of_polynomials
+
+#==================================================
+import sys
+sys.path.append("../../../")
+from Modulo_Sequence.Modulo_Polynomial import *
+
+import sys
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    N,M=map(int,input().split())
+    F=list(map(int,input().split()))
+    G=list(map(int,input().split()))
+
+    F=Modulo_Polynomial(F,N)
+    G=Modulo_Polynomial(G,M)
+
+    Q,R=divmod(F,G)
+
+    Q.reduce(); R.reduce()
+    Q=Q.Poly if Q.Poly!=[0] else []
+    R=R.Poly if R.Poly!=[0] else []
+
+    print(len(Q), len(R))
+    write(" ".join(map(str,Q))); print()
+    write(" ".join(map(str,R)))
+
+#==================================================
+verify()
