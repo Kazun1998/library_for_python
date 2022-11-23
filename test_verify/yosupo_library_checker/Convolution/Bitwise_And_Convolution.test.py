6c6f6914610aa340486baff094b607b7f8fffe50
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-11-23 16:01:19 +0900

    add verify of bitwise and convolution

---
+++ test_verify/yosupo_library_checker/Convolution/Bitwise_And_Convolution.test.py
@@ -0,0 +1,22 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/bitwise_and_convolution
+
+#==================================================
+from Convolution.AND_Convolution import *
+
+import sys
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    import sys
+    input=sys.stdin.readline
+    write=sys.stdout.write
+
+    N=int(input())
+    A=list(map(int,input().split()))
+    B=list(map(int,input().split()))
+    write(" ".join(map(str,Convolution_AND(A,B))))
+
+#==================================================
+verify()
