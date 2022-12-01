a81ee5a32ea78a6dd62621141f958671b5055cad
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-12-02 01:16:54 +0900

    add verify of product of sequence of polynomial

---
+++ test_verify/yosupo_library_checker/Polynomial/Product_of_Polynomial_Sequence.test.py
@@ -0,0 +1,26 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/product_of_polynomial_sequence
+
+#==================================================
+from Modulo_Sequence.Modulo_Polynomial import *
+
+import sys
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    N=int(input())
+    A=[None]*N
+
+    global Mod; Mod=998244353
+    global Calc; Calc=Calculator()
+
+    D=0
+    for i in range(N):
+        d,*A[i]=map(int,input().split())
+        D+=d
+
+    write(" ".join(map(str, Calc.Multiple_Convolution(*A))))
+
+#==================================================
+verify()
