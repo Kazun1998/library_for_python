b084cae8299074c1777ed6423de303071c1f1e5f
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2022-12-25 04:35:06 +0900

    現状を更新

--- Union_Find/Potentialized_Union_Find.py
+++ Union_Find/Potentialized_Union_Find.py
@@ -1,6 +1,9 @@
+"""
+バグあり
+"""
+
 class Potentilized_Union_Find():
-    __slots__=["n","parents","rank","edges","pot"]
-    def __init__(self,N):
+    def __init__(self, N, calc, e, inv):
         """ 0,1,...,N-1 を要素として初期化する.
 
         N: 要素数
@@ -10,49 +13,70 @@ class Potentilized_Union_Find():
         self.rank=[0]*N
         self.edges=[0]*N
         self.pot=[0]*N
+        self.valid=[True]*N
+        self.__group_number=N
+
+        self.calc=calc
+        self.diff=lambda u,v:self.calc(u, self.inv(v))
+        self.e=e
+        self.inv=inv
+
 
     def find(self, x):
         """ 要素 x の属している族を調べる.
 
         x: 要素
         """
-        V=[]
-        while self.parents[x]>=0:
-            V.append(x)
-            x=self.parents[x]
 
-        for v in V:
-            self.pot[v]+=self.pot[self.parents[v]]
-            self.parents[v]=x
-        return x
+        if self.parents[x]<0:
+            return x
+
+        r=x
+        data=[]
+        while self.parents[r]>=0:
+            data.append(r)
+            r=self.parents[r]
+
+        for x in data:
+            self.pot[x]=self.calc(self.pot[x], self.pot[r])
+            self.parents[x]=r
+
+        return r
 
     def union(self, x, y, u):
-        """ 要素 x,y を同一視し, U(x)-U(y)=u という情報を加える.
+        """ 要素 x,y を同一視し, U(y)-U(x)=u という情報を加える.
 
         x,y: 要素
         """
 
-        u+=self.pot[x]-self.pot[y]
-        x=self.find(x)
-        y=self.find(y)
+        u=self.calc(u, self.diff(self.pot[x],self.pot[y]))
+
+        x=self.find(x); y=self.find(y)
 
         if x==y:
+            self.valid[x]&=self.diff(self.pot[y],self.pot[x])==u
             self.edges[x]+=1
-            assert self.potential(x,y)==u
             return
 
-        if self.rank[x]>self.rank[y]:
-            x,y,u=y,x,-u
-
-        self.edges[x]+=self.edges[y]+1
-        self.edges[y]=0
+        if self.rank[x]<self.rank[y]:
+            x,y=y,x
+            u=self.inv(u)
+        elif self.rank[x]==self.rank[y]:
+            self.rank[x]+=1
 
         self.parents[x]+=self.parents[y]
         self.parents[y]=x
 
-        if self.rank[x]==self.rank[y]:
-            self.rank[x]+=1
-        self.pot[x]=u
+        self.edges[x]+=self.edges[y]+1
+        self.edges[y]=0
+
+        self.valid[x]&=self.valid[y]
+
+        self.pot[y]=u
+
+        self.__group_number-=1
+
+        return
 
     def size(self, x):
         """ 要素 x の属している族の要素の数.
@@ -61,11 +85,11 @@ class Potentilized_Union_Find():
         """
         return -self.parents[self.find(x)]
 
-    def potential(self, x, y):
-        """ y を基準にした x のポテンシャル"""
+    def potential_energy(self, x, y):
+        """ x を基準にした y のポテンシャルエネルギー"""
 
-        if self.same(x,y):
-            return self.pot[x]-self.pot[y]
+        if self.same(x,y) and self.is_valid(x):
+            return self.diff(self.pot[y], self.pot[x])
         else:
             return None
 
@@ -92,32 +116,33 @@ class Potentilized_Union_Find():
         """
         return self.edges[self.find(x)]
 
-    def is_forest(self, x):
+    def is_valid(self, x):
+        """ x が属している族のポテンシャルが妥当かどうかを判定する. """
+
+        return self.valid[self.find(x)]
+
+    def is_tree(self, x):
         """ 要素 x が属する族が森かどうかを判定する.
 
         x: 要素
         """
         return self.size(x)==self.edges[self.find(x)]+1
 
-    def forest_count(self):
+    def tree_count(self):
         """ 森になっている属の個数を求める.
         """
 
-        X=0
-        for g in self.roots():
-            if self.is_forest(g):
-                X+=1
-        return X
+        return sum(self.is_tree(g) for g in self.representative())
 
-    def roots(self):
-        """ 族の名前のリスト
+    def representative(self):
+        """ 代表元の名前のリスト
         """
         return [i for i, x in enumerate(self.parents) if x < 0]
 
     def group_count(self):
         """ 族の個数
         """
-        return len(self.roots())
+        return self.__group_number
 
     def all_group_members(self):
         """ 全ての族の出力
@@ -135,3 +160,6 @@ class Potentilized_Union_Find():
         return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
 
     __repr__=__str__
+
+from operator import *
+U=Potentilized_Union_Find(4, add, 0, neg)
