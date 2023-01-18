9aa0c7147e6212362ba2fc13bb3c4bbc1a66ed41
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2023-01-18 19:44:08 +0900

    Sparse Binary Indexed Tree を 0-indexed 化

--- Binary_Indexed_Tree/Sparse_Binary_Indexed_Tree.py
+++ Binary_Indexed_Tree/Sparse_Binary_Indexed_Tree.py
@@ -1,113 +1,114 @@
 class Sparse_Binary_Indexed_Tree():
-    def __init__(self, N, calc, unit, inv, index=1):
+    def __init__(self, N, op, unit, inv):
         """ calc を演算とする最高の添字が N になるような Sparse Binary Indexed Tree を作成
         calc: 演算 (2変数関数, 可換群)
         unit: 群 calc の単位元 (x+e=e+x=xを満たすe)
         inv : 群 calc の逆元 (1変数関数, x+inv(x)=inv(x)+x=e をみたす inv(x))
         """
-        self.calc=calc
+        self.op=op
         self.unit=unit
         self.inv=inv
-        self.index=index
-
-        d=max(1,(N+(1-index)-1).bit_length())
-        k=2**d
-
-        self.num=k
-        self.depth=d
+        self.N=N
+        self.log=max(N.bit_length()-1, 1)
         self.data={}
 
-    def index_number(self, k, index=1):
+    def get(self, k):
         """ 第 k 要素の値を出力する.
         k    : 数列の要素
         index: 先頭の要素の番号
         """
-        return self.sum(k,k,index)
+        return self.sum(k,k)
 
-    def add(self, k, x, index=1):
+    def add(self, k, x):
         """ 第 k 要素に x を加え, 更新を行う.
         k    : 数列の要素
         x    : 加える値
-        index: 先頭の要素の番号
-        right:「左から」が「右から」になる
         """
-        p=k+(1-index)
-        while p<=self.num:
-            self.data[p]=self.calc(self.data.get(p,self.unit),x)
+
+        data=self.data; op=self.op
+        p=k+1
+        while p<=self.N:
+            data[p]=op(data.get(p, self.unit), x)
             p+=p&(-p)
 
-    def update(self, k, x, index=1):
+    def update(self, k, x):
         """ 第 k 要素を x に変え, 更新を行う.
         k: 数列の要素
         x: 更新後の値
         """
 
-        a=self.index_number(k,index)
-        y=self.calc(self.inv(a),x)
+        a=self.get(k)
+        y=self.op(self.inv(a), x)
 
-        self.add(k,y,index)
+        self.add(k,y)
 
-    def sum(self, From, To, index=1):
-        """ 第 From 要素から第 To 要素までの総和を求める.
-        ※From!=1を使うならば, 群でなくてはならない.
-        From : 始まり
-        To   : 終わり
-        index: 先頭の要素の番号
+    def sum(self, l, r):
+        """ 第 l 要素から第 r 要素までの総和を求める.
+        ※ l != 0 を使うならば, 群でなくてはならない.
+        l: 始まり
+        r: 終わり
         """
-        alpha=max(1,From+(1-index))
-        beta=min(self.num,To+(1-index))
 
-        if alpha>beta:
+        l=l+1 if 0<=l else 1
+        r=r+1 if r<self.N else self.N
+
+        if l>r:
             return self.unit
-        elif alpha==1:
-            return self.__section(beta)
+        elif l==1:
+            return self.__section(r)
         else:
-            return self.calc(self.inv(self.__section(alpha-1)),self.__section(beta))
+            return self.op(self.inv(self.__section(l-1)), self.__section(r))
 
-    def __section(self,x):
-        """ B[1]+...+B[x] を求める. """
+    def __section(self, x):
+        """ B[0]+...+B[x] を求める. """
+        data=self.data; op=self.op
         S=self.unit
-        while x:
-            S=self.calc(self.data.get(x,self.unit),S)
+        while x>0:
+            S=op(data.get(x, self.unit), S)
             x-=x&(-x)
         return S
 
     def all_sum(self):
-        return self.data.get(self.num,self.unit)
+        return self.sum(0, self.N-1)
 
-    def binary_search(self, cond, index=1):
-        """ cond(B[1]+...+B[k]) を満たす最小の k を返す.
+    def binary_search(self, cond):
+        """ cond(B[0]+...+B[k]) が True となるような最小の k を返す.
 
         cond: 単調増加
 
-        ※ cond(uint)=True の場合の返り値は index-1
-        ※ cond(B[1]+...+B[k]) なる k が存在しない場合の返り値は self.num+index
+        ※ cond(unit)=True の場合の返り値は -1 とする.
+        ※ cond(B[0]+...+B[k]) なる k が (0<=k<N に) 存在しない場合の返り値は N とする.
         """
 
         if cond(self.unit):
-            return index-1
+            return -1
 
         j=0
-        r=self.num
-        t=r
-        data=self.data
+        r=self.N
+        t=1<<self.log
+        data=self.data; op=self.op
         alpha=self.unit
 
-        for _ in range(self.depth+1):
-            if j+t<=self.num:
-                beta=self.calc(alpha,data.get(j+t,self.unit))
+        while t>0:
+            if j+t<=self.N:
+                beta=op(alpha, data.get(j+t, self.unit))
                 if not cond(beta):
                     alpha=beta
                     j+=t
             t>>=1
 
-        return j+index
+        return j
 
-    def __getitem__(self,index):
-        if isinstance(index,int):
-            return self.index_number(index,self.index)
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self.get(index)
         else:
-            return [self.index_number(t,self.index) for t in index]
+            return [self.get(t) for t in index]
 
-    def __setitem__(self,index,val):
-        self.update(index,val,self.index)
+    def __setitem__(self, index, val):
+        self.update(index, val)
+
+    def __iter__(self):
+        for k in range(self.N):
+            yield self.sum(k, k)
+
