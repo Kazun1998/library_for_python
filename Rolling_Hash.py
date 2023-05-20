96ddcda6695755444d56cb7b5f1a271164fc597b
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2023-05-20 13:26:54 +0900

    Double_Hasher を追加

--- Rolling_Hash.py
+++ Rolling_Hash.py
@@ -205,3 +205,35 @@ class Hasher():
 
     def decode(self, S):
         pass
+
+#=================================================
+class Double_Hasher():
+    def __init__(self, length, base, mod0, mod1, type=0):
+        self.length=length
+        self.base=base
+        self.mod0=mod0
+        self.mod1=mod1
+        self.type=type
+
+        self.power0=pw0=[1]*length
+        self.power1=pw1=[1]*length
+        for i in range(1, length):
+            pw0[i]=(base*pw0[i-1])%mod0
+            pw1[i]=(base*pw1[i-1])%mod1
+
+    def __repr__(self):
+        return "length: {}\nbase: {}\nmod: {}".format(self.length, self.base, self.mod)
+
+    def encode(self, S):
+        code0=0; code1=0
+        N=len(S)
+        for i in range(N):
+            if self.type:
+                code0+=ord(S[i])*self.power0[N-1-i]%self.mod0
+                code1+=ord(S[i])*self.power1[N-1-i]%self.mod1
+            else:
+                code0+=S[i]*self.power0[N-1-i]%self.mod0
+                code1+=S[i]*self.power1[N-1-i]%self.mod1
+
+        code0%=self.mod0; code1%=self.mod1
+        return code1*self.mod0+code0
