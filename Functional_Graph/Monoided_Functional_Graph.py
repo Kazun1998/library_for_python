8f694e34faaf70fc91f454d1fb4493c2971268f5
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2023-06-18 16:22:50 +0900

    calc を op に変更

--- Functional_Graph/Monoided_Functional_Graph.py
+++ Functional_Graph/Monoided_Functional_Graph.py
@@ -1,17 +1,17 @@
 class Monoided_Functional_Graph:
-    def __init__(self, N, K, calc, unit):
+    def __init__(self, N, K, op, unit):
         """ 有向辺にモノイドの重みを乗せた Functional Graph を生成する.
 
         Args:
             N : 頂点数 (0,1,2,...,N-1 を生成する)
             K : 計算に必要な最大の指数
-            calc : モノイドの演算 calc(new, old)
+            op : モノイドの演算 op(new, old)
             unit : 単位元
         """
 
         self.N=N
         self.K=K
-        self.calc=calc
+        self.op=op
         self.unit=unit
 
         self.out=[list(range(N))]
@@ -24,7 +24,7 @@ class Monoided_Functional_Graph:
     def build(self):
         K=self.K>>1
         N=self.N
-        calc=self.calc
+        op=self.op
         while K:
             A=self.out[-1]; X=[0]*N
             B=self.value[-1]; Y=[0]*N
@@ -32,21 +32,21 @@ class Monoided_Functional_Graph:
             for i in range(N):
                 p=A[i]; x=B[i]
                 q=A[p]; y=B[p]
-                X[i]=q; Y[i]=calc(y,x)
+                X[i]=q; Y[i]=op(y,x)
 
             self.out.append(X)
             self.value.append(Y)
             K>>=1
 
-    def calculate(self, v, k):
+    def opulate(self, v, k):
         x=self.unit
-        calc=self.calc
+        op=self.op
         out=self.out
         value=self.value
         d=0
         while k:
             if k&1:
-                x=calc(value[d][v], x)
+                x=op(value[d][v], x)
                 v=out[d][v]
             k>>=1; d+=1
         return x
