d3043bfd34d6dbc449df46bba11a4efbdad9a16b
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2023-07-08 02:59:35 +0900

    Point クラスの中身を修正

--- Geometric/Point.py
+++ Geometric/Point.py
@@ -1,174 +1,154 @@
+from Common import Common, sign, compare
 from math import sqrt,sin,cos,tan,asin,acos,atan2,pi,floor,gcd
 
-def compare(x,y,ep):
-    """ x,y の大小比較をする. ただし, ep の誤差は同一視する.
+class Point(Common):
+    __slots__=("x", "y")
 
-    [Input]
-    x,y: float
-    ep: float
-
-    [Output]
-    x>y: 1
-    x=y: 0
-    x<y: -1
-    """
-
-    if x-y>ep: return 1
-    elif x-y<-ep: return -1
-    else: return 0
-
-def max_ep(*X):
-    e=-1
-    for x in X:
-        if x.ep>e:e=x.ep
-    return e
-
-class Point():
-    __slots__=["x","y","id"]
-    ep=1e-9
-
-    def __init__(self,x=0,y=0):
-        self.x=x
-        self.y=y
-        self.id=0
-
-    def sign(self,a):
-        return compare(a,0,self.ep)
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 
     #文字列
     def __str__(self):
-        return "({}, {})".format(self.x,self.y)
+        return "({}, {})".format(self.x, self.y)
 
     __repr__=__str__
 
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
     #Bool
     def __bool__(self):
-        return self.sign(self.x)!=0 or self.sign(self.y)!=0
+        return sign(self.x) != 0 or sign(self.y) != 0
 
     #等号
-    def __eq__(self,other):
-        return self.sign(self.x-other.x)==0 and self.sign(self.y-other.y)==0
+    def __eq__(self, other):
+        return compare(self.x, other.x) == 0 and compare(self.y, other.y) == 0
 
     #不等号
-    def __ne__(self,other):
-        return not self==other
+    def __ne__(self, other):
+        return not self == other
 
     #比較(<)
-    def __lt__(self,other):
-        T=self.sign(self.x-other.x)
-        if T:
-            return T<0
+    def __lt__(self, other):
+        sign_x = compare(self.x, other.x)
+
+        if sign_x == -1:
+            return True
+        elif sign_x == 1:
+            return False
         else:
-            return self.sign(self.y-other.y)<0
+            return compare(self.y, other.y) == -1
 
     #比較(<=)
-    def __le__(self,other):
-        return self<other or self==other
+    def __le__(self, other):
+        return self < other or self == other
 
     #比較(>)
-    def __gt__(self,other):
-        return other<self
+    def __gt__(self, other):
+        return other < self
 
     #比較(>=)
-    def __ge__(self,other):
-        return other<=self
+    def __ge__(self, other):
+        return other <= self
 
     #正と負
     def __pos__(self):
         return self
 
     def __neg__(self):
-        return Point(-self.x,-self.y)
+        return Point(-self.x, -self.y)
 
     #加法
-    def __add__(self,other):
-        return Point(self.x+other.x,self.y+other.y)
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
 
-    def __iadd__(self,other):
-        self.x+=other.x
-        self.y+=other.y
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
         return self
 
     #減法
-    def __sub__(self,other):
-        return Point(self.x-other.x,self.y-other.y)
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
 
-    def __isub__(self,other):
-        self.x-=other.x
-        self.y-=other.y
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
         return self
 
     #乗法
-    def __mul__(self,other):
-        x,y=self.x,self.y
-        u,v=other.x,other.y
-        return Point(x*u-y*v,x*v+y*u)
+    def __mul__(self, other):
+        x, y = self
+        u, v = other
+        return Point(x * u - y * v, x * v + y * u)
 
     def __imul__(self, other):
-        return other*self
+        return other * self
 
-    def __rmul__(self,other):
-        if isinstance(other,(int,float)):
-            return Point(other*self.x,other*self.y)
+    def __rmul__(self, other):
+        if isinstance(other, (int, float)):
+            return Point(other * self.x, other * self.y)
 
     #除法
-    def __truediv__(self,other):
+    def __truediv__(self, other):
         if other==0:
             raise ZeroDivisionError
-        return Point(self.x/other,self.y/other)
+        return Point(self.x / other, self.y / other)
 
     #絶対値
     def __abs__(self):
-        return sqrt(self.x*self.x+self.y*self.y)
+        x,y = self
+        return sqrt(self.x * self.x + self.y * self.y)
 
     norm=__abs__
 
     def norm_2(self):
-        return self.x*self.x+self.y*self.y
+        return self.x * self.x + self.y * self.y
 
     #回転
-    def rotate(self,theta):
-        x,y=self.x,self.y
-        s,c=sin(theta),cos(theta)
-        return Point(c*x-s*y,s*x+c*y)
-
-    def __iter__(self):
-        yield self.x
-        yield self.y
+    def rotate(self, theta):
+        x, y = self
+        s, c = sin(theta), cos(theta)
+        return Point(c * x - s * y, s * x + c * y)
 
     def __hash__(self):
-        return hash((self.x,self.y))
+        return hash((self.x, self.y))
 
-    def latticization(self,delta=1e-7):
+    def latticization(self, delta):
         """ 格子点に十分近いならば, その格子点に吸い寄せる"""
 
-        if abs(self.x-floor(self.x+0.5))<delta and abs(self.y-floor(self.y+0.5))<delta:
-            self.x=floor(self.x+0.5)
-            self.y=floor(self.y+0.5)
+        if compare(self.x, floor(self.x + 0.5)) == 0 and compare(self.y, floor(self.y + 0.5)) == 0:
+            self.x = floor(self.x + 0.5)
+            self.y = floor(self.y + 0.5)
 
     def normalization(self):
-        a=abs(self)
-        self.x/=a
-        self.y/=a
+        a = abs(self)
+        self.x /= a
+        self.y /= a
 
     def normal_unit_vector(self):
         """ 単位法線ベクトルを求める"""
 
         assert self
         d=self.norm()
-        return Point(-self.y/d,self.x/d)
+        return Point(-self.y / d, self.x / d)
 
-    def dot(self,other):
-        return self.x*other.x+self.y*other.y
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
 
-    def det(self,other):
-        return self.x*other.y-self.y*other.x
+    def det(self, other):
+        return self.x * other.y - self.y * other.x
 
     def arg(self):
-        return atan2(self.y,self.x)
+        return atan2(self.y, self.x)
 
     def copy(self):
-        return Point(self.x,self.y)
+        return Point(self.x, self.y)
+
+def Distance(A,B):
+    return abs(A-B)
 
 def iSP(A,B,C):
     """ A->B->C と進んだときの進行方向を見る. ※ B が中心
@@ -293,6 +273,16 @@ def Argument_Sort_by_Index(L):
     L: 点のリスト
     """
 
+    ep=max_ep(*L)
+    def position(P):
+        m=compare(P.y,0,ep)
+        if m==-1:
+            return -1
+        elif m==0 and compare(P.x,0,ep)>=0:
+            return 0
+        else:
+            return 1
+
     def merge(a,b):
         I=[]
 
@@ -307,18 +297,25 @@ def Argument_Sort_by_Index(L):
                 I.append(b[ib])
                 ib+=1
 
-        for i in range(ia,la):
-            I.append(a[i])
-
-        for i in range(ib,lb):
-            I.append(b[i])
+        I.extend(a[ia:])
+        I.extend(b[ib:])
 
         return I
 
     def sorting(a):
-        if len(a)==1:
+        if len(a)<=1:
             return a
         else:
             return merge(sorting(a[:len(a)//2]),sorting(a[len(a)//2:]))
 
-    return sorting(list(range(len(L))))
+    I=[]; J=[]; K=[]
+    for i in range(len(L)):
+        t=position(L[i])
+        if t==1:
+            I.append(i)
+        elif t==0:
+            J.append(i)
+        else:
+            K.append(i)
+
+    return sorting(K)+J+sorting(I)
