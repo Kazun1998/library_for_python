526acc224313c13fd712a45e7c60f35fbc625b13
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2023-08-04 01:36:20 +0900

    見た目の修正

--- Integer/Quotient_Reminder.py
+++ Integer/Quotient_Reminder.py
@@ -1,66 +1,102 @@
 #商列挙
 def Quotient_Range(N):
-    """Nで割った商の可能性を全て列挙する.
+    """ N で割った商の可能性を全て列挙する.
 
     [Input]
-    N:正整数
+    N: 正整数
 
     [Output]
-    X:リスト
-    Xの各要素(k,x,y) は x<=i<=y であることと, floor(N/i)=k が同値であることを表す.
+    X: リスト
+    Xの各要素 (t, x, y) は x <= k <= y であることと, floor(N/k) = t が同値であることを表す.
+
+    [Note]
+    各 (t, x, y) に対して, x <= k <= y の範囲において, N mod k は等差数列になる.
     """
-    X=[]
+    X = []
 
-    M=1
-    while M*M<=N:
-        X.append((N//M,M,M))
-        M+=1
+    # Step 1: k<=sqrt(N) の範囲について k を全探索する.
+    k = 1
+    while k * k <= N:
+        X.append((N//k, k, k))
+        k += 1
 
-    for i in range(M,0,-1):
-        L=N//(i+1)+1
-        R=N//i
+    # Step 2: t<=sqrt(N) の範囲において, floor(N/k)=t を満たす k の範囲を求める.
+    for t in range(k, 0, -1):
+        l = N//(t + 1) + 1
+        r = N//t
 
-        if L<=R and X[-1][-1]<L:
-            X.append((N//L,L,R))
+        if (l <= r) and (X[-1][1] < l):
+            X.append((t, l, r))
+
     return X
 
-def Reminder_Enumeration(N,r):
+def Quotient_Range_Yielder(N):
+    """ N で割った商の可能性を全て列挙する.
+
+    [Input]
+    N: 正整数
+
+    [Output]
+    X: リスト
+    Xの各要素 (t, x, y) は x <= k <= y であることと, floor(N/k) = t が同値であることを表す.
+
+    [Note]
+    各 (t, x, y) に対して, x <= k <= y の範囲において, N mod k は等差数列になる.
+    """
+
+    # Step 1: k<=sqrt(N) の範囲について k を全探索する.
+    k = 1
+    while k * k <= N:
+        yield (N//k, k, k)
+        k += 1
+
+    # Step 2: t<=sqrt(N) の範囲において, floor(N/k)=t を満たす k の範囲を求める.
+    prev_l = k - 1
+    for t in range(k, 0, -1):
+        l = N//(t + 1) + 1
+        r = N//t
+
+        if (l <= r) and (prev_l < l):
+            yield (t, l, r)
+            prev_l = l
+
+def Reminder_Enumeration(N, r):
     """ N を q 割った余りが r になる q を全て列挙する.
 
     N: 正整数
-    r: 非負整数, N!=r
+    r: 非負整数, N != r
     """
 
-    assert N!=r,"無限個あります."
+    assert N != r, "無限個あります."
 
-    k=1
-    X=[];Y=[]
-    N-=r
-    while k*k<=N:
-        if N%k==0:
-            if k>r:
+    k = 1
+    X = []; Y = []
+    N -= r
+    while k * k <= N:
+        if N % k == 0:
+            if k > r:
                 X.append(k)
-            if k*k!=N and N//k>r:
+            if k * k != N and N//k > r:
                 Y.append(N//k)
-        k+=1
-    return X+Y[::-1]
+        k += 1
+    return X + Y[::-1]
 
 def Next_Remainder(x, p, r):
     """ x 以上で p で割って r 余る整数のうち, 最小の整数を求める.
 
     """
 
-    if x%p<=r:
-        return (x//p)*p+r
+    if x % p <= r:
+        return (x//p) * p + r
     else:
-        return (x//p+1)*p+r
+        return (x//p + 1) * p + r
 
 def Previous_Remainder(x, p, r):
     """ x 以下で p で割って r 余る整数のうち, 最大の整数を求める.
 
     """
 
-    if r<=x%p:
-        return (x//p)*p+r
+    if r <= x % p:
+        return (x//p) * p + r
     else:
-        return (x//p-1)*p+r
+        return (x//p - 1) * p + r
