5be935d2844e076505cbc83c77808acb2d2ee4f6
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2023-08-11 03:29:05 +0900

    Line class 修正

--- Geometric/Line.py
+++ Geometric/Line.py
@@ -70,36 +70,35 @@ class Ray():
         return self.begin - self.end
 
 class Line():
-    __slots__=["begin","end","id"]
+    __slots__ = ("begin", "end")
 
     ep=1e-9
-    def __init__(self,P,Q):
-        """2点 P, Q (P!=Q) を通る直線を生成する.
+    def __init__(self, P, Q):
+        """2点 P, Q (P != Q) を通る直線を生成する.
 
         P,Q: Point
         """
-        assert P!=Q
-        self.begin=P
-        self.end=Q
-        self.id=4
+        assert P != Q
+        self.begin = P
+        self.end = Q
 
     def __str__(self):
-        return "[Line] {}, {}".format(self.begin,self.end)
+        return f"[Line] {self.begin}, {self.end}"
 
-    __repr__=__str__
+    __repr__ = __str__
 
-    def __eq__(self,other):
-        a=self.begin; b=self.end; c=other.begin; d=other.end
-        return (b-a).det(c-d)==0 and (b-a).det(c-a)==0
+    def __eq__(self, other):
+        a = self.begin; b = self.end; c = other.begin; d = other.end
+        return sign((b-a).det(c-d)) == 0 and sign((b-a).det(c-a)) == 0
 
-    def __contains__(self,point):
-        return abs(iSP(self.begin,point,self.end))!=1
+    def __contains__(self, point):
+        return abs(iSP(self.begin, point, self.end))!=1
 
     def vectorize(self):
-        return self.end-self.begin
+        return self.end - self.begin
 
     def counter_vectorize(self):
-        return self.begin-self.end
+        return self.begin - self.end
 
 #=== 生成
 def Line_from_General_Form(a,b,c):
