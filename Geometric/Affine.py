2b9925ac133c3d575ec04b190a3d867d042a829c
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2023-08-11 13:38:44 +0900

    Affine クラスの属性名変更

--- Geometric/Affine.py
+++ Geometric/Affine.py
@@ -5,55 +5,55 @@ from Triangle import *
 from Polygon import *
 
 class Affine():
-    def __init__(self,Mat=[[1,0],[0,1]],Vec=[0,0]):
-        self.Mat=Mat
-        self.Vec=Vec
+    def __init__(self, mat = [[1, 0], [0, 1]], vec=[0, 0]):
+        self.mat = mat
+        self.vec = vec
 
     def __str__(self):
-        return "Mat: {}, Vec:{}".format(self.Mat,self.Vec)
+        return f"Mat: {self.mat}, Vec: {self.vec}"
 
-    __repr__=__str__
+    __repr__ = __str__
 
     def __pos__(self):
         return self
 
     def __neg__(self):
-        [[a,b],[c,d]]=self.Mat
-        x,y=self.Vec
+        [[a,b],[c,d]]=self.mat
+        x,y=self.vec
         return Affine([[-a,-b],[-c,-d]],[-x,-y])
 
     def __add__(self,other):
         M=[[0,0],[0,0]]
-        M[0][0]=self.Mat[0][0]+other.Mat[0][0]
-        M[0][1]=self.Mat[0][1]+other.Mat[0][1]
-        M[1][0]=self.Mat[1][0]+other.Mat[1][0]
-        M[1][1]=self.Mat[1][1]+other.Mat[1][1]
+        M[0][0]=self.mat[0][0]+other.mat[0][0]
+        M[0][1]=self.mat[0][1]+other.mat[0][1]
+        M[1][0]=self.mat[1][0]+other.mat[1][0]
+        M[1][1]=self.mat[1][1]+other.mat[1][1]
 
-        v=[self.Vec[0]+other.Vec[0], self.Vec[1]+other.Vec[1]]
+        v=[self.vec[0]+other.vec[0], self.vec[1]+other.vec[1]]
 
         return Affine(M,v)
 
     def __sub__(self,other):
         M=[[0,0],[0,0]]
-        M[0][0]=self.Mat[0][0]-other.Mat[0][0]
-        M[0][1]=self.Mat[0][1]-other.Mat[0][1]
-        M[1][0]=self.Mat[1][0]-other.Mat[1][0]
-        M[1][1]=self.Mat[1][1]-other.Mat[1][1]
+        M[0][0]=self.mat[0][0]-other.mat[0][0]
+        M[0][1]=self.mat[0][1]-other.mat[0][1]
+        M[1][0]=self.mat[1][0]-other.mat[1][0]
+        M[1][1]=self.mat[1][1]-other.mat[1][1]
 
-        v=[self.Vec[0]-other.Vec[0], self.Vec[1]-other.Vec[1]]
+        v=[self.vec[0]-other.vec[0], self.vec[1]-other.vec[1]]
 
         return Affine(M,v)
 
     def __mul__(self,other):
         M=[[0,0],[0,0]]
-        M[0][0]=self.Mat[0][0]*other.Mat[0][0]+self.Mat[0][1]*other.Mat[1][0]
-        M[0][1]=self.Mat[0][0]*other.Mat[0][1]+self.Mat[0][1]*other.Mat[1][1]
-        M[1][0]=self.Mat[1][0]*other.Mat[0][0]+self.Mat[1][1]*other.Mat[1][0]
-        M[1][1]=self.Mat[1][0]*other.Mat[0][1]+self.Mat[1][1]*other.Mat[1][1]
+        M[0][0]=self.mat[0][0]*other.mat[0][0]+self.mat[0][1]*other.mat[1][0]
+        M[0][1]=self.mat[0][0]*other.mat[0][1]+self.mat[0][1]*other.mat[1][1]
+        M[1][0]=self.mat[1][0]*other.mat[0][0]+self.mat[1][1]*other.mat[1][0]
+        M[1][1]=self.mat[1][0]*other.mat[0][1]+self.mat[1][1]*other.mat[1][1]
 
         v=[
-            self.Mat[0][0]*other.Vec[0]+self.Mat[0][1]*other.Vec[1]+self.Vec[0],
-            self.Mat[1][0]*other.Vec[0]+self.Mat[1][1]*other.Vec[1]+self.Vec[1]
+            self.mat[0][0]*other.vec[0]+self.mat[0][1]*other.vec[1]+self.vec[0],
+            self.mat[1][0]*other.vec[0]+self.mat[1][1]*other.vec[1]+self.vec[1]
             ]
         return Affine(M,v)
 
@@ -71,11 +71,11 @@ class Affine():
         return  B
 
     def __eq__(self,other):
-        return self.Mat==other.Mat and self.Vec==other.Vec
+        return self.mat==other.mat and self.vec==other.vec
 
     def inverse(self):
-        [[a,b],[c,d]]=self.Mat
-        x,y=self.Vec
+        [[a,b],[c,d]]=self.mat
+        x,y=self.vec
 
         det=a*d-b*c
         p,q,r,s=d/det,-b/det,-c/det,a/det
@@ -84,14 +84,14 @@ class Affine():
     def integerization(self,delta=1e-7):
         for i in [0,1]:
             for j in [0,1]:
-                if abs(self.Mat[i][j]-floor(self.Mat[i][j]+0.5))<delta:
-                    self.Mat[i][j]=floor(self.Mat[i][j]+0.5)
+                if abs(self.mat[i][j]-floor(self.mat[i][j]+0.5))<delta:
+                    self.mat[i][j]=floor(self.mat[i][j]+0.5)
 
-        if abs(self.Vec[0]-floor(self.Vec[0]+0.5))<delta:
-            self.Vec[0]=floor(self.Vec[0]+0.5)
+        if abs(self.vec[0]-floor(self.vec[0]+0.5))<delta:
+            self.vec[0]=floor(self.vec[0]+0.5)
 
-        if abs(self.Vec[1]-floor(self.Vec[1]+0.5))<delta:
-            self.Vec[1]=floor(self.Vec[1]+0.5)
+        if abs(self.vec[1]-floor(self.vec[1]+0.5))<delta:
+            self.vec[1]=floor(self.vec[1]+0.5)
 
     def __getitem__(self,shape):
         return Action(self,shape)
@@ -105,8 +105,8 @@ def Action(A: Affine,S):
     """
 
     if isinstance(S,Point):
-        [[a,b],[c,d]]=A.Mat
-        u,v=A.Vec
+        [[a,b],[c,d]]=A.mat
+        u,v=A.vec
         return Point(a*S.x+b*S.y+u, c*S.x+d*S.y+v)
     elif isinstance(S,Segment):
         return Segment(Action(A,S.begin),Action(A,S.end))
@@ -126,7 +126,7 @@ def Translation(x,y):
     """ (x,y) だけ平行移動させるアフィン変換を生成する.
     """
 
-    return Affine(Vec=[x,y])
+    return Affine(vec=[x,y])
 
 def Point_Reflection(x=0,y=0):
     """ 点 (x,y) に関する対称移動をするアフィン変換を生成する.
