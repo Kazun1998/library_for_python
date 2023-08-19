6aa2baab48f0dd760b71e616e11ea2f9ce985e7c
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2023-08-20 01:43:27 +0900

    誤り push を修正

--- Integer/Modulo.py
+++ Integer/Modulo.py
@@ -1,7 +1,6 @@
 #法 p の原始根
 def Primitive_Root(p):
     """Z/pZ上の原始根を見つける
-
     p:素数
     """
     if p==2:
@@ -10,54 +9,57 @@ def Primitive_Root(p):
         return 3
     if p==10**9+7:
         return 5
-
     fac=[]
     q=2
     v=p-1
-
     while v>=q*q:
         e=0
         while v%q==0:
             e+=1
             v//=q
-
         if e>0:
             fac.append(q)
         q+=1
-
     if v>1:
         fac.append(v)
-
     g=2
     while g<p:
         if pow(g,p-1,p)!=1:
             return None
-
         flag=True
         for q in fac:
             if pow(g,(p-1)//q,p)==1:
                 flag=False
                 break
-
         if flag:
             return g
 
         g+=1
 
+#拡張ユークリッドの互除法
+def Extend_Euclid(a: int, b: int):
+    """ax+by=gcd(a, b) を満たす (x, y, gcd(a, b)) を 1 つ求める.
+    a,b:整数
+    """
+    from math import gcd
+    g = gcd(a, b)
+    if g == 0:
+        return (1, 0, 0)
+
+    x = pow(a//g, -1, b//g)
+    y = - (a*x-g) // b
+    return (x, y, g)
+
+
 def Modulo_Inverse(a, m):
     """ (mod m) における逆元を求める.
-
     Args:
         a (int): mod m の元
         m (int): 法
-
     Returns:
         int: 可逆元が存在するならばその値, 存在しないのであれば -1
     """
-
     try:
         return pow(a, -1, m)
     except ValueError:
         return -1
-
-
