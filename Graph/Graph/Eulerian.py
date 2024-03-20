b9782f7a0d3c7323b30bc40d20d9785b5f6e5eb8
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2024-03-20 22:03:14 +0900

    add: Find_Eulerian_Trail 構築

--- Graph/Graph/Eulerian.py
+++ Graph/Graph/Eulerian.py
@@ -10,10 +10,53 @@ def Is_Semi_Eulerian_Graph(G: Graph):
     """ グラフ G が準オイラーグラフかどうかを判定する. """
     return len([v for v in range(G.order()) if G.degree(v) % 2 == 0]) == 2 and Is_Connected(G)
 
-#Euler 路を見つける
+#Euler (閉) 路を見つける
 def Find_Eulerian_Trail(G: Graph):
-    pass
+    N = G.order()
 
-#Euler閉路を見つける
-def Find_Eulerian_Cycle(G: Graph):
-    pass
+    remain = [G.degree(v) for v in range(N)]
+
+    odd_vertex = [v for v in range(N) if remain[v] % 2 == 1]
+    if len(odd_vertex) > 2:
+        return { 'vertex': None, 'edge': None }
+    elif len(odd_vertex) == 2:
+        start, goal = odd_vertex
+    else:
+        start = goal = 0
+
+    adj = [[edge for edge in G.adjacent[x]] for x in range(N)]
+    seen = set()
+
+    def dfs(start):
+        path = []
+
+        x = start
+        while True:
+            if not adj[x]:
+                break
+
+            y, i = adj[x].pop()
+            if i in seen:
+                continue
+
+            seen.add(i)
+            path.append((x, y, i))
+            remain[x] -= 1; remain[y] -= 1
+            x = y
+        return path
+
+    stack = dfs(start)
+    edge = []
+    vertex = [goal]
+    while stack:
+        u, _, j = stack.pop()
+        vertex.append(u)
+        edge.append(j)
+
+        if remain[u]:
+            stack.extend(dfs(u))
+
+    if len(edge) == G.size():
+        return { 'vertex': vertex, 'edge': edge }
+    else:
+        return { 'vertex': None, 'edge': None }
