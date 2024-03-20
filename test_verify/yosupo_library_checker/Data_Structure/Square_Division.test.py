b4054454f08cd2ad74fd1e8ab04368feb6d7aeaf
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2024-03-21 00:25:53 +0900

    add: 平方分割の verify を実装

---
+++ test_verify/yosupo_library_checker/Data_Structure/Square_Division.test.py
@@ -0,0 +1,27 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/static_range_sum
+
+#==================================================
+from Division.Square_Divion import *
+
+import sys
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    from operator import add
+
+    N, Q = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    S = Square_Division(A, add, 0)
+    ans = [0] * Q
+
+    for q in range(Q):
+        l, r = map(int, input().split())
+        ans[q] = S.product(l, r - 1)
+
+    write("\n".join(map(str, ans)))
+
+#==================================================
+verify()
