7962b7ae1f96c57133db34e1b6493ba85a68c4be
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2024-03-22 23:31:16 +0900

    add: 有向グラフ Eulerian Trail を実装

---
+++ test_verify/yosupo_library_checker/Graph/Directed_Eulerian_Trail.test.py
@@ -0,0 +1,32 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/eulerian_trail_directed
+
+#==================================================
+from Graph.Digraph.DiGraph import Digraph as Directed_Graph
+from Graph.DigGraph.Eulerian import Find_Directed_Eulerial_Trail
+
+import sys
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    N, M = map(int, input().split())
+
+    D = Directed_Graph(N)
+    for j in range(M):
+        u, v = map(int, input().split())
+        D.add_arc(u, v, j)
+
+    euler = Find_Directed_Eulerial_Trail(D)
+
+    if euler['vertex'] is None:
+        print("No")
+    else:
+        print('Yes')
+        print(*euler['vertex'])
+        print(*euler['edge'])
+
+#==================================================
+T = int(input())
+for _ in range(T):
+    verify()
