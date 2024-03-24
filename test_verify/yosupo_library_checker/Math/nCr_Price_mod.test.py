709085a15ad36c4e1dd11eff3164f279b70ea4e9
Kazun1998 <67512870+Kazun1998@users.noreply.github.com>
2024-03-24 15:03:14 +0900

    add: nCr (mod prime) のテストを追加

---
+++ test_verify/yosupo_library_checker/Math/nCr_Price_mod.test.py
@@ -0,0 +1,25 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/binomial_coefficient_prime_mod
+
+#==================================================
+from Enumeration import Enumeration
+
+import sys
+input=sys.stdin.readline
+write=sys.stdout.write
+
+#==================================================
+def verify():
+    T, m = map(int, input().split())
+
+    global Mod; Mod = m
+    global fact, fact_inv
+    fact, fact_inv = Factor_with_inverse(min(m, 10 ** 7))
+
+    ans = [0] * T
+    for t in range(T):
+        n, k = map(int, input().split())
+        ans[t] = nCr(n, k)
+    return ans
+
+#==================================================
+write("\n".join(map(str, verify())))
