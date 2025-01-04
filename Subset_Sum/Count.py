bc3e135fb0c912210ba3b60da53ea360a7ffe7af
Kazun1998 <hkazu1998@gmail.com>
2025-01-04 13:43:29 +0900

    refactor: Count_Subset_Sum のクラス化

--- Subset_Sum/Count.py
+++ Subset_Sum/Count.py
@@ -1,33 +1,47 @@
-def Subset_Sum_Zero_One_Count(A, S, Mod = None):
-    """ A の多重部分集合で, 和が s (0 <= s <= S) になる数を求める.
+class Count_Sumset_Sum:
+    @staticmethod
+    def zero_one_dp(A: list, S: int, Mod: int = None) -> list[int]:
+        """ A の多重部分集合のうち, 合計が 0,1,...,S になるものの数 (を Mod で割った余り) を求める (取り出す要素が異なれば別のものとみなす).
 
-    Mod: 余り (None のときは余りを取らない)
+        Args:
+            A (list): 多重集合
+            S (int): 目標となる合計
+            Mod (int, optional): 割る余り. Defaults to None.
 
-    計算量: O(|A| S)
-    """
+        Returns:
+            list[int]: 長さ (S+1) の配列. 第 k~(0 <= k <= S) 項には, 合計が k になる多重部分集合の数が格納されている.
+        """
 
-    DP = [0] * (S + 1); DP[0] = 1
+        dp = [0] * (S + 1); dp[0] = 1
 
-    for a in A:
-        for x in range(S, a - 1, -1):
-            DP[x] += DP[x - a]
+        for a in A:
+            for x in range(S, a - 1, -1):
+                dp[x] += dp[x - a]
 
-        if Mod is not None:
+            if Mod is None:
+                continue
+
             for x in range(a, S + 1):
-                DP[x] %= Mod
+                dp[x] %= Mod
 
-    return DP
+        return dp
 
-def Subset_Sum_Plus_Minus_One_Count(A, K, Mod = None):
-    """ 以下を満たす A の分割 X, Y の個数を求める: sum(X) - sum(Y) = K.
+    @classmethod
+    def plus_minus_dp(cls, A: list, S: int, Mod: int = None) -> list[int]:
+        """ A の多重部分集合のうち, 合計が 0,1,...,S になるものの数 (を Mod で割った余り) を求める (取り出す要素が異なれば別のものとみなす).
 
-    計算量: O(|A|(sum(A)+K))
-    """
+        Args:
+            A (list): 多重集合
+            S (int): 目標となる合計
+            Mod (int, optional): 割る余り. Defaults to None.
 
-    A = list(map(abs, A))
+        Returns:
+            list[int]: 長さ (S+1) の配列. 第 k~(0 <= k <= S) 項には, 合計が k になる多重部分集合の数が格納されている.
+        """
 
-    L = K + sum(A)
-    if (L < 0) or (L % 2 == 1):
-        return 0
+        A = list(map(abs, A))
+        K = S + sum(A)
+        if (K < 0) or (K % 2 == 1):
+            return 0
 
-    return Subset_Sum_Zero_One_Count(A, L // 2, Mod)[-1]
+        return cls.zero_one_dp(A, K // 2, Mod)[-1]
