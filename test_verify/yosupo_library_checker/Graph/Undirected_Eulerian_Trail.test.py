5f9f56fcea2da612ecd5f1d16396a01a001284fa
Kazun1998 <hkazu1998@gmail.com>
2025-01-04 20:03:58 +0900

    fix: テスト改修

--- test_verify/yosupo_library_checker/Graph/Undirected_Eulerian_Trail.test.py
+++ test_verify/yosupo_library_checker/Graph/Undirected_Eulerian_Trail.test.py
@@ -1,13 +1,10 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/eulerian_trail_undirected
 
 #==================================================
-import sys
-sys.path.append('~/Graph/Digraph')
-
 from Graph.Graph.Graph import Graph as Undirected_Graph
 from Graph.Graph.Eulerian import *
 
-
+import sys
 input=sys.stdin.readline
 write=sys.stdout.write
 
