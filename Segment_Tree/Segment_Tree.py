d432fec216acfd244d0f80a521ed1ef137fbb76e
Kazun1998 <hkazu1998@gmail.com>
2025-02-22 11:32:25 +0900

    add: TypeVar 導入

--- Segment_Tree/Segment_Tree.py
+++ Segment_Tree/Segment_Tree.py
@@ -1,9 +1,14 @@
-class Segment_Tree():
-    def __init__(self, L, op, unit):
-        """ op を演算とするリスト L の Segment Tree を作成
+from typing import TypeVar, Generic, Callable, Iterator
 
-        op: 演算 (2変数関数, Monoid)
-        unit: Monoid op の単位元 (xe=ex=xを満たすe)
+M = TypeVar('M')
+class Segment_Tree(Generic[M]):
+    def __init__(self, L: list[M], op: Callable[[M, M], M], unit: M):
+        """ op を演算とする初期状態 L の Segment Tree を生成する.
+
+        Args:
+            L (list[M]): 初期状態
+            op (Callable[[M, M], M]): 演算
+            unit (M): M の単位元
         """
         self.op=op
         self.unit=unit
@@ -19,18 +24,26 @@ class Segment_Tree():
         for i in range(k-1,0,-1):
             data[i]=op(data[i<<1], data[i<<1|1])
 
-    def get(self, k):
-        """ 第 k 要素を取得
+    def get(self, k: int) -> M:
+        """ 第 k 要素を取得する.
+
+        Args:
+            k (int): 要素の場所
+
+        Returns:
+            M: 第 k 要素
         """
         assert 0<=k<self.N,"添字が範囲外"
         return self.data[k+self.N]
 
-    def update(self, k, x):
-        """第k要素をxに変え,更新を行う.
+    def update(self, k: int, x: M) -> None:
+        """ 第 k 要素を x に変え, 更新する.
 
-        k:数列の要素
-        x:更新後の値
+        Args:
+            k (int): 要素の場所
+            x (M): 更新後の第 k 要素
         """
+
         assert 0<=k<self.N,"添字が範囲外"
         m=k+self.N
 
@@ -41,7 +54,19 @@ class Segment_Tree():
             m>>=1
             data[m]=op(data[m<<1], data[m<<1|1])
 
-    def product(self, l, r, left_closed=True,right_closed=True):
+    def product(self, l: int, r: int, left_closed: bool = True, right_closed: bool = True) -> M:
+        """ 第 l 要素から第 r 要素までの総積を求める.
+
+        Args:
+            l (int): 左端
+            r (int): 右端
+            left_closed (bool, optional): False にすると, 左端が開区間になる. Defaults to True.
+            right_closed (bool, optional): False にすると, 右端が開区間になる. Defaults to True.
+
+        Returns:
+            M: 第 l 要素から第 r 要素までの積
+        """
+
         L=l+self.N+(not left_closed)
         R=r+self.N+(right_closed)
 
@@ -63,18 +88,23 @@ class Segment_Tree():
 
         return op(vL,vR)
 
-    def all_product(self):
+    def all_product(self) -> M:
         return self.data[1]
 
-    def max_right(self, left, cond):
-        """ 以下の2つをともに満たす x の1つを返す.\n
-        (1) r=left or cond(data[left]*data[left+1]*...*data[r-1]): True
-        (2) r=N or cond(data[left]*data[left+1]*...*data[r]): False
-        ※ cond が単調減少の時, cond(data[left]*...*data[r-1]) を満たす最大の r となる.
+    def max_right(self, left: int, cond: Callable[[int], bool]) -> int:
+        """ 以下の2つをともに満たす r の1つを返す.\n
+        (1) r=left or cond(data[left]*data[left+1]*...*data[r-1]): True\n
+        (2) r=N or cond(data[left]*data[left+1]*...*data[r]): False\n
 
-        cond:関数(引数が同じならば結果も同じ)
-        cond(unit): True
-        0<=left<=N
+        ※ cond が単調減少の時, cond(data[left]*...*data[r-1]) を満たす最大の r となる.\n
+        ※ cond(unit) = True を課す.
+
+        Args:
+            left (int): 左端
+            cond (Callable[[int], bool]): 条件
+
+        Returns:
+            int: r
         """
 
         assert 0<=left<=self.N,"添字が範囲外"
@@ -104,16 +134,22 @@ class Segment_Tree():
             left+=1
         return self.N
 
-    def min_left(self, right, cond):
-        """ 以下の2つをともに満たす y の1つを返す.\n
-        (1) l=right or cond(data[l]*data[l+1]*...*data[right-1]): True
-        (2) l=0 or cond(data[l-1]*data[l]*...*data[right-1]): False
-        ※ cond が単調増加の時, cond(data[l]*...*data[right-1]) を満たす最小の l となる.
+    def min_left(self, right: int, cond: Callable[[int], bool]) -> int:
+        """ 以下の 2 つをともに満たす l の1つを返す.\n
+        (1) l=right or cond(data[l]*data[l+1]*...*data[right-1]): True\n
+        (2) l=0 or cond(data[l-1]*data[l]*...*data[right-1]): False\n
 
-        cond: 関数(引数が同じならば結果も同じ)
-        cond(unit): True
-        0<=right<=N
+        ※ cond が単調増加の時, cond(data[l]*...*data[right-1]) を満たす最小の l となる.\n
+        ※ cond(unit) = True を課す.
+
+        Args:
+            right (int): 右端
+            cond (Callable[[int], bool]): 条件
+
+        Returns:
+            int: l
         """
+
         assert 0<=right<=self.N,"添字が範囲外"
         assert cond(self.unit),"単位元が条件を満たさない."
 
@@ -141,12 +177,12 @@ class Segment_Tree():
             sm=op(data[right], sm)
         return 0
 
-    def __getitem__(self,k):
+    def __getitem__(self, k: int) -> M:
         return self.get(k)
 
-    def __setitem__(self,k,x):
+    def __setitem__(self, k: int, x: M) -> None:
         return self.update(k,x)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[M]:
         for i in range(self.n):
             yield self.get(i)
