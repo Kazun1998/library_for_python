84366d27068bccb217e9f925b6c41cc63b1d6ac3
Kazun1998 <hkazu1998@gmail.com>
2025-03-13 23:53:34 +0900

    refactoring: SCC

--- Strongly_Connected_Components.py
+++ Strongly_Connected_Components.py
@@ -1,101 +1,118 @@
 class Strongly_Connected_Components:
-    def __init__(self, N):
-        """ N 頂点の有向グラフを生成する. """
-        self.N=N
-        self.arc=[[] for _ in range(N)]
-        self.rev=[[] for _ in range(N)]
+    def __init__(self, N: int):
+        """ N 頂点の有向グラフを生成する.
 
-    def add_vertex(self):
-        """ 頂点を追加する.
-
+        Args:
+            N (int): 頂点数
         """
 
-        self.N+=1
+        self.arc: list[list[int]] = [[] for _ in range(N)]
+        self.rev: list[list[int]] = [[] for _ in range(N)]
+
+    @property
+    def N(self):
+        return len(self.arc)
+
+    def add_vertex(self) -> int:
+        """ 1 頂点を追加する.
+
+        Returns:
+            int: 追加した頂点の番号
+        """
+
         self.arc.append([])
         self.rev.append([])
-        return self.N-1
+        return self.N - 1
 
-    def add_vertices(self, k=1):
+    def add_vertices(self, k: int = 1) -> list[int]:
         """ 頂点を k 個追加する.
 
-        k: int
+        Args:
+            k (int, optional): 追加する頂点数. Defaults to 1.
+
+        Returns:
+            list[int]: 追加した k 個の頂点の番号
         """
 
-        self.N+=k
         self.arc.extend([[] for _ in range(k)])
         self.rev.extend([[] for _ in range(k)])
-        return list(range(self.N-k, self.N))
+        return list(range(self.N - k, self.N))
 
-    def add_arc(self, source, target):
-        """ source から target へ結ぶ有向辺を追加する.
+    def add_arc(self, source: int, target: int):
+        """ source から target への弧を結ぶ.
 
+        Args:
+            source (int): 弧の始点
+            target (int): 弧の終点
         """
 
         self.arc[source].append(target)
         self.rev[target].append(source)
 
-    def decomposition(self, mode=0):
-        """有向グラフを強連結成分に分解
+    def decomposition(self):
+        """有向グラフを強連結成分に分解"""
 
-        Mode:
-        0 (defalt) ---各強連結成分の頂点のリスト
-        1        ---各頂点が属している強連結成分の番号
-        2        ---0, 1 の両方
+        group = [0] * self.N
+        D = [False] * self.N
+        O = []
 
-        ※ 0 or 2で帰ってくるリストは各強連結成分に関してトポロジカルソートである.
-        """
-
-        G=[0]*self.N
-        D=[0]*self.N
-        O=[]
-
+        # 1st DFS
         for v in range(self.N):
-            if G[v]:
+            if group[v] != 0:
                 continue
 
-            S=[~v, v]
+            stack = [~v, v]
 
-            while S:
-                v=S.pop()
-                if v>=0:
-                    if G[v]==-1:
+            while stack:
+                v = stack.pop()
+                if v >= 0:
+                    # in
+                    if group[v] == -1:
                         continue
 
-                    G[v]=-1
+                    group[v] = -1
                     for w in self.arc[v]:
-                        if not G[w]:
-                            S.append(~w)
-                            S.append(w)
+                        if not group[w]:
+                            stack.append(~w)
+                            stack.append(w)
                 else:
-                    v=~v
-                    if not D[v]:
-                        D[v]=1
-                        O.append(v)
+                    # out
+                    v = ~v
+                    if D[v]:
+                        continue
 
-        K=0
+                    D[v] = True
+                    O.append(v)
+
+        components = []
+        # 2nd DFS
         for v in reversed(O):
-            if G[v]!=-1:
+            if group[v] != -1:
                 continue
 
-            S=[v]
-            G[v]=K
+            stack = [v]
+            component = [v]
+            group[v] = len(components)
 
-            while S:
-                w=S.pop()
+            while stack:
+                w = stack.pop()
                 for u in self.rev[w]:
-                    if G[u]==-1:
-                        G[u]=K
-                        S.append(u)
-            K+=1
+                    if group[u] != -1:
+                        continue
 
-        if mode==0 or mode==2:
-            R=[[] for _ in range(K)]
-            for i in range(self.N):
-                R[G[i]].append(i)
+                    group[u] = len(components)
+                    component.append(u)
+                    stack.append(u)
 
-        if mode==0:
-            return R
-        elif mode==1:
-            return G
-        else:
-            return R,G
+            components.append(component)
+
+        self.__components = components
+        self.__group = group
+
+    @property
+    def components(self) -> list[list[int]]:
+        return self.__components
+
+    @property
+    def group(self) -> list[int]:
+        return self.__group
