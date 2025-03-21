11626ccd4ac1fe163f1460d3f9c64910f75af90f
Kazun1998 <hkazu1998@gmail.com>
2025-03-22 01:01:16 +0900

    add: Nimber 実装 (WIP)

--- Nimber.py
+++ Nimber.py
@@ -1,27 +1,39 @@
-class Nimber():
-    def __init__(self,x):
-        assert x>=0
-        self.x=x
+class Nimber:
+    def __init__(self, x: int = 0):
+        self.__x = x
 
     def __str__(self):
-        return "Nimber({})".format(self.x)
+        return str(self.__x)
 
     def __repr__(self):
-        return "Nimber({})".format(self.x)
+        return f"{self.__class__.__name__}({self.__x})"
 
-    #正, 負
-    def __pos__(self):
-        return self
+    def __add__(self, other: "Nimber") -> "Nimber":
+        return Nimber(self.__x ^ other.__x)
 
-    def __neg__(self):
-        return self
+    def __mul__(self, other: "Nimber") -> "Nimber":
+        pass
 
-    #加法
-    def __add__(self,other):
-        return Nimber(self.x^other.x)
+    @classmethod
+    def __mul_calc(cls, x: int, y: int) -> int:
+        pass
 
-    #減法
-    def __sub__(self,other):
-        return self+other
+    @staticmethod
+    def __floor_log(x: int) -> int:
+        return x.bit_length() - 1
 
-    #乗法
+    @staticmethod
+    def __separate(x: int, lv: int):
+        upper = x >> (1 << (lv - 1))
+        lower = x ^ (upper << (1 << (lv - 1)))
+        return upper, lower
+
+    @classmethod
+    def level(cls, x: int) -> int:
+        if x == 0:
+            return 0
+        else:
+            return cls.__floor_log(cls.__floor_log(cls.__x)) + 1
+
+    def __hash__(self) -> int:
+        return hash(self.__x)
