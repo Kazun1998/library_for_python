127ee8a2e245abe02cf9dcd33e897847718ef742
Kazun1998 <hkazu1998@gmail.com>
2025-05-16 23:27:09 +0900

    refactor: 線形漸化式改修

--- Modulo_Matrix/Modulo_Sequence.py
+++ Modulo_Matrix/Modulo_Sequence.py
@@ -1,47 +1,43 @@
 from Modulo_Matrix import *
 
+def Linear_Recurrence_Sequence_Matrix(p: list[int]) -> Modulo_Matrix:
+    """ 線形漸化式を表す行列を求める. 線形漸化式は以下の形式である.
+        a_{k + d} = p[0] a[k] + p[1] a[k + 1] + ... + p[d - 1] a[n + d - 1]
+
+    Args:
+        p (list[int]): 線形漸化式の係数のリスト
+
+    Returns:
+        Modulo_Matrix: 線形漸化式を表す行列
+    """
+    A = [p[::-1]]
+    d = len(p)
+    for i in range(d - 1):
+        A.append([1 if j == i else 0 for j in range(d)])
+    return Modulo_Matrix(A)
+
 #漸化式と行列
-def Linear_Recurrence_Sequence_Value(p,x,N):
-    """線形漸化式の第N項を求める.
+def Linear_Recurrence_Sequence_Value(p: list[int], x: list[int], N: int) -> int:
+    """ 以下の形で表される線形漸化式 (a_n) の第 n 項を求める. ただし, d := len(x) とする.
+        a_k = x_k (k = 0, 1, ..., d - 1)
+        a_{k + d} = p[0] a[k] + p[1] a[k + 1] + ... + p[d - 1] a[n + d - 1]
 
-    p:漸化式 (d=|p| とする.)
-    x:第0項から第(d-1)項までの値
-    N:第N項
+    Args:
+        p (list[int]): 線形漸化式の係数のリスト
+        x (list[int]): 先頭 d 項
+        N (int): 求める項の番号
 
-    線形漸化式は x[n+d]=p[0]x[n]+p[1]x[n+1]+...+p[d-1]x[n+d-1] とする.
+    Returns:
+        int: a_N
     """
 
-    assert len(p)==len(x)
-    d=len(p)
+    assert len(p) == len(x)
+    d = len(p)
 
-    if N<d:
+    if N < d:
         return x[N]
 
-    A=[p[::-1]]
-    for i in range(d-1):
-        A.append([1 if j==i else 0 for j in range(d)])
-    A=Modulo_Matrix(A)
-    v=Modulo_Matrix([[y] for y in x])
+    A = Linear_Recurrence_Sequence_Matrix(p)
 
-    X=0
-    aa=pow(A,N-d+1).ele[0][::-1]
-
-    for i in range(d):
-        X+=aa[i]*x[i]
-
-    return X%Mod
-
-def Linear_Recurrence_Sequence_Matrix(p):
-    """線形漸化式から行列を作る.
-
-    p:漸化式 (d=|p|)
-    Mod:法
-
-    線形漸化式は x[n+d]=p[0]x[0]+p[1]x[1]+...+p[d-1] x[d-1] とする.
-    """
-
-    A=[p[::-1]]
-    d=len(p)
-    for i in range(d-1):
-        A.append([1 if j==i else 0 for j in range(d)])
-    return Modulo_Matrix(A)
+    top_row_rev = pow(A, N - d + 1).ele[0][::-1]
+    return sum(top_row_rev[i] * x[i] for i in range(d)) %Mod
