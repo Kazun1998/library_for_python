f6bb5d55d939e151e0b8673bfbe2b289700bb62f
Kazun1998 <hkazu1998@gmail.com>
2025-05-24 12:57:42 +0900

    refactor: Montmort_Number

--- Sequence/Montmort_Number.py
+++ Sequence/Montmort_Number.py
@@ -1,18 +1,25 @@
-def Montmort_Number(N, Mod=None):
-    """ k=0,1,...,N に関して, k 要素撹乱順列の個数を求める.
+def Montmort_Number(N: int, Mod: int = None) -> list[int]:
+    """ k = 0, 1, 2, ..., N に対して, k 要素撹乱順列の個数 (を Mod で割った余り) を求める.
+
+    Args:
+        N (int): 求める k の上限
+        Mod (int, optional): 整数を設定すると, 各値は個数を Mod で割った余りになる. Defaults to None.
+
+    Returns:
+        list[int]: 長さ (N + 1) のリスト. 第 k 要素は k 要素撹乱順列の個数 (を Mod で割った余り) である.
     """
-    if N<0:
+
+    if N < 0:
         return []
-    elif N==0:
+    elif N == 0:
         return [0]
 
-    X=[0]*(N+1)
-    if Mod==None:
-        for k in range(2, N+1):
-            X[k]=k*X[k-1]+(-1 if k%2 else 1)
+    dp = [0] * (N + 1)
+    if Mod is None:
+        for k in range(2, N + 1):
+            dp[k] = k * dp[k - 1] + (-1 if k % 2 else 1)
     else:
         for k in range(2, N+1):
-            X[k]=(k*X[k-1]+(-1 if k%2 else 1))%Mod
+            dp[k] = (k * dp[k - 1] + (-1 if k % 2 else 1)) % Mod
 
-    return X
-
+    return dp
