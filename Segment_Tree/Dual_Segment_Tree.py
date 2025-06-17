0e4cafbe68528a0f1d5637fb864d0156c038b89e
Kazun1998 <hkazu1998@gmail.com>
2025-06-17 23:45:14 +0900

    add: 双対 Segment Tree に Typing を追加

--- Segment_Tree/Dual_Segment_Tree.py
+++ Segment_Tree/Dual_Segment_Tree.py
@@ -1,15 +1,17 @@
 from typing import TypeVar, Generic, Callable
 
-F = TypeVar('F')
-class Dual_Segment_Tree(Generic[F]):
-    def __init__(self, A: list[F], comp: Callable[[F, F], F], id: F):
-        """ comp を演算とするリスト L の Dual Segment Tree を作成
+Func = TypeVar('Func')
+class Dual_Segment_Tree(Generic[Func]):
+    def __init__(self, A: list[Func], comp: Callable[[Func, Func], Func], id: Func):
+        """ Func における comp を作用素の合成とするリスト A に関する双対 Segment Tree を生成する.
 
-        comp : 作用素
-        id : 単位元
+        Args:
+            A (list[Func]): 元となるリスト
+            comp (Callable[[Func, Func], Func]): 作用素の合成
+            id (Func): Func
 
         [注記]
-        更新は左から. つまり, comp(new, old) となる.
+            comp における更新は左から. つまり, comp(new, old) となる.
         """
 
         self.__comp = comp
@@ -23,11 +25,11 @@ class Dual_Segment_Tree(Generic[F]):
         self.__size = k
 
     @property
-    def comp(self) -> Callable[[F, F], F]:
+    def comp(self) -> Callable[[Func, Func], Func]:
         return self.__comp
 
     @property
-    def id(self) -> F:
+    def id(self) -> Func:
         return self.__id
 
     @property
@@ -67,13 +69,13 @@ class Dual_Segment_Tree(Generic[F]):
             self._propagate_at(m >> h)
 
     #作用
-    def action(self, l: int, r: int, alpha: F, left_closed: bool = True, right_closed: bool = True):
+    def action(self, l: int, r: int, alpha: Func, left_closed: bool = True, right_closed: bool = True):
         """ 第 l 要素から第 r 要素までの各要素に alpha を作用させる.
 
         Args:
             l (int): 左端
             r (int): 右端
-            alpha (F): 作用させる値
+            alpha (Func): 作用させる値
             left_closed (bool, optional): False にすると, 左端が開区間になる. Defaults to True.
             right_closed (bool, optional): False にすると, 右端が開区間になる. Defaults to True.
         """
@@ -123,21 +125,21 @@ class Dual_Segment_Tree(Generic[F]):
             self._propagate_at(m)
 
     #取得
-    def get(self, k: int) -> F:
+    def get(self, k: int) -> Func:
         """ 第 k 要素を取得する.
 
         Args:
             k (int): 要素の番号
 
         Returns:
-            F: 第 k 要素
+            Func: 第 k 要素
         """
 
         m = k + self.size
         self._propagate_above(m)
         return self.lazy[m]
 
-    def __getitem__(self, index: int) -> F:
+    def __getitem__(self, index: int) -> Func:
         m = index + self.size
         self._propagate_above(m)
         return self.lazy[m]
