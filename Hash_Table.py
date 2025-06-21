0261eacf63692b1d5f4de53d68edb4c862a97b5e
Kazun1998 <hkazu1998@gmail.com>
2025-06-22 01:07:02 +0900

    refactor: Hash_Table

--- Hash_Table.py
+++ Hash_Table.py
@@ -1,46 +1,53 @@
-class Hash_Table:
-    __slots__=("table", "seed")
+from random import randint
+from typing import TypeVar, Generic, Optional, Hashable
+
+V = TypeVar('V')
+class Hash_Table(Generic[V]):
+    __slots__ = ("__table", "__seed")
 
     def __init__(self):
-        from random import randint
-        self.table={}
-        self.seed=randint(-(1<<63)-1, (1<<63)-1)
+        self.__table: dict[Hashable, V] = {}
+        self.__seed = randint(-(1 << 63) - 1, (1 << 63) - 1)
 
-    def get(self, key, default=None):
-        return self.table.get(key^self.seed, default)
+    @property
+    def seed(self) -> int:
+        return self.__seed
 
-    def __len__(self):
-        return len(self.table)
+    def get(self, key: Hashable, default: V = None) -> Optional[V]:
+        return self.__table.get(key^self.seed, default)
 
-    def __getitem__(self, key):
-        h=key^self.seed
-        if h in self.table:
-            return self.table[h]
+    def __len__(self) -> int:
+        return len(self.__table)
+
+    def __getitem__(self, key: Hashable) -> V:
+        k = hash(key) ^ self.seed
+        if k in self.__table:
+            return self.__table[k]
         else:
             raise KeyError(key)
 
-    def __setitem__(self, key, value):
-        self.table[key^self.seed]=value
+    def __setitem__(self, key: Hashable, value: V):
+        self.__table[hash(key) ^ self.seed] = value
 
     def __iter__(self):
         return self.keys()
 
-    def __contains__(self, key):
-        return key^self.seed in self.table
+    def __contains__(self, key: Hashable) -> bool:
+        return hash(key) ^ self.seed in self.__table
 
     def clear(self):
-        self.table.clear()
+        self.__table.clear()
 
     def keys(self):
-        for alpha in self.table:
-            yield alpha^self.seed
+        for alpha in self.__table:
+            yield alpha ^ self.seed
 
     def values(self):
-        return self.table.values()
+        return self.__table.values()
 
     def items(self):
-        for alpha in self.table:
-            yield (alpha^self.seed, self.table[alpha])
+        for alpha in self.__table:
+            yield (alpha ^ self.seed, self.__table[alpha])
 
 class Hash_Set:
     def __init__(self):
