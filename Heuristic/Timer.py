8dffbf89237ca059eb3f4ad5f5d133426f3a9647
Kazun1998 <hkazu1998@gmail.com>
2025-06-29 13:19:27 +0900

    refactor: Timer

--- Heuristic/Timer.py
+++ Heuristic/Timer.py
@@ -2,20 +2,40 @@ from time import time
 
 class Timer:
     def __init__(self, time_limit: float, period: int = 1) -> "Timer":
-        self.started_at = time()
-        self.time_limit = time_limit
-        self.period = period
-        self.counter = period
-        self.t = 0
+        self.__started_at = time()
+        self.__time_limit = time_limit
+        self.__period = period
+        self.__counter = period
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(time_limit={self.time_limit}, self.period={self.period})"
 
     def __bool__(self) -> bool:
-        self.counter -= 1
-        if self.counter:
+        self.__counter -= 1
+        if self.__counter:
             return True
 
-        self.counter = self.period
+        self.__counter = self.period
         return time() - self.started_at <= self.time_limit
 
     @property
+    def started_at(self) -> float:
+        return self.__started_at
+
+    @property
+    def period(self) -> int:
+        return self.__period
+
+    @property
+    def time_limit(self) -> float:
+        return self.__time_limit
+
+    @property
     def time(self) -> float:
+        """ 経過時間を出力する.
+
+        Returns:
+            float: 経過時間
+        """
+
         return time() - self.started_at
